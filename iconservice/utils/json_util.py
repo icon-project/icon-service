7127d97d1cf19eb955e4275ea7e727018e1d973f
inwonKim <inwonkim@icon.foundation>
2018-04-23 18:40:47 +0900

    Add docstrings.(#10)

--- iconservice/utils/json_util.py
+++ iconservice/utils/json_util.py
@@ -2,6 +2,11 @@ from ..base.address import Address
 
 
 def convert_dict_values(json_dict: dict) -> dict:
+    """Convert json into appropriate format.
+
+    :param json_dict:
+    :return:
+    """
     json_dictionary = {}
 
     for key in json_dict:
@@ -14,6 +19,14 @@ def convert_dict_values(json_dict: dict) -> dict:
 
 
 def convert_value(str_value_with_type: str):
+    """Convert str value into specified type.
+
+    :param str_value_with_type:
+    example1: '123 -> int'
+    example2: '[1,2,3] -> int_array'
+    example3: '[True, False, False] -> bool_array'
+    :return:
+    """
     specified_type = str_value_with_type.split(" -> ")[-1]
     separator_index = str_value_with_type.rfind(" -> ")
     value = str_value_with_type[:separator_index]
@@ -41,16 +54,31 @@ def convert_value(str_value_with_type: str):
 
 
 def _convert_into_str_array(str_array: str) -> str:
+    """Used inside convert_value function. This function will remove quotes, double quotes and space from the string.
+
+    :param str_array:
+    :return:
+    """
     return str_array[1:-1].replace('"', '').replace("'", '').replace(' ', '')
 
 
 def string_to_bool(str_bool: str) -> bool:
+    """Convert str_bool to bool value. This function will returns False When argument is 'False'
+
+    :param str_bool:
+    :return:
+    """
     if bool(str_bool) is False or str_bool == "False":
         return False
     return True
 
 
 def get_str_array_from_str(string: str):
+    """Returns str array.
+
+    :param string: string that enclose the array with only strings as members.
+    :return:
+    """
     quotes_count = 0
     quotes = string[0]
     str_array = []
