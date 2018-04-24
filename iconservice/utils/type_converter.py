0120b4b8d22784edc59c16fbea5cd2cc92d42a8b
inwonKim <inwonkim@icon.foundation>
2018-04-24 18:16:10 +0900

    Implemented TypeConverter class.(#10)

--- iconservice/utils/type_converter.py
+++ iconservice/utils/type_converter.py
@@ -13,12 +13,91 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from ..base.address import Address
 
 
 class TypeConverter(object):
+
+    CONST_INT = "int"
+    CONST_STRING = "string"
+    CONST_BOOL = "bool"
+    CONST_ADDRESS = "address"
+    CONST_INT_ARRAY = "int[]"
+    CONST_STRING_ARRAY = "string[]"
+    CONST_BOOL_ARRAY = "bool[]"
+    CONST_ADDRESS_ARRAY = "address[]"
+    CONST_BYTES = "bytes"
+
     def __init__(self, type_table: dict=None) -> None:
         self.param_type_table = type_table
 
     def convert(self, params: dict) -> dict:
-        pass
+        return self.convert_dict_values(params)
+
+    def convert_dict_values(self, json_dict: dict) -> dict:
+        """Convert json into appropriate format.
+
+        :param json_dict:
+        :return:
+        """
+        json_dictionary = {}
+
+        for key in json_dict:
+            if isinstance(json_dict[key], dict):
+                json_dictionary[key] = self.convert_dict_values(json_dict[key])
+            else:
+                json_dictionary[key] = self.convert_value(key, json_dict[key])
+
+        return json_dictionary
+
+    def convert_value(self, key, value):
+        """Convert str value into specified type.
+
+        :param key:
+        :param value:
+        :return:
+        """
+        try:
+            value_type = self.param_type_table[key]
+            if value_type == TypeConverter.CONST_INT:
+                return int(value, 0)
+            elif value_type == TypeConverter.CONST_STRING:
+                return value
+            elif value_type == TypeConverter.CONST_BOOL:
+                return value == "True" or value is True
+            elif value_type == TypeConverter.CONST_ADDRESS:
+                return Address(value[:2], bytes.fromhex(value[2:]))
+            elif value_type == TypeConverter.CONST_INT_ARRAY:
+                return value
+            elif value_type == TypeConverter.CONST_BOOL_ARRAY:
+                return value
+            elif value_type == TypeConverter.CONST_STRING_ARRAY:
+                return value
+            elif value_type == TypeConverter.CONST_ADDRESS_ARRAY:
+                return [Address(a[:2], bytes.fromhex(a[2:])) for a in value]
+            elif value_type == TypeConverter.CONST_BYTES:
+                return value.encode('utf-8')
+        except KeyError:
+            return value
+
+    @staticmethod
+    def _convert_into_str_array(str_array: str) -> list:
+        """Used inside convert_value function. This function will remove quotes, double quotes and space from the string.
+
+        :param str_array:
+        :return:
+        """
+        string_array = str_array[1:-1]
+        return string_array.replace('"', '').replace("'", '').replace(' ', '').split(",")
+
+    @staticmethod
+    def string_to_bool(str_bool: str) -> bool:
+        """Convert str_bool to bool value. This function will returns False When argument is 'False'
+
+        :param str_bool: ex) 'True', 'False'
+        :return:
+        """
+        if bool(str_bool) is False or str_bool == "False":
+            return False
+        return True
 
--- tests/test_utils_type_converter.py
+++ tests/test_utils_type_converter.py
@@ -27,25 +27,67 @@ class TestTypeConverter(unittest.TestCase):
             "from": "address",
             "to": "address",
             "value": "int",
-            "addresses": "address[]"
+            "addresses": "address[]",
+            "bool": "bool",
+            "bool2": "bool",
+            "bool_array": "bool[]",
+            "int_array": "int[]",
+            "str_array1": "string[]",
+            "str_array2": "string[]",
+            "data": "bytes"
         }
+
         _from = f'hx{"0" * 40}'
         _to = f'hx{"1" * 40}'
         _value = 0x1234
         _addresses = ['hx' + str(i) * 40 for i in range(5)]
-
+        _bool = True
+        _bool2 = False
+        _bool_array = [True, False, True, False]
+        _int_array = [0x1, 0x2, 0x3, 0x1234, 0x5]
+        _str_array1 = ["asdf", 'qwer', 'qwer', 'zxcv']
+        _unknown = "asdf"
         converter = TypeConverter(type_table)
+        _data = b'1234'
 
         params = {
             "from": str(_from),
             "to": str(_to),
             "value": hex(_value),
-            "addresses": _addresses
+            "addresses": _addresses,
+            "int_array": _int_array,
+            "bool": "True",
+            "bool2": "False",
+            "bool_array": _bool_array,
+            "str_array1": _str_array1,
+            "data": {
+                "value": "0x1234",
+                "data": _data.decode('utf-8')
+            },
+            "unknown": _unknown
         }
 
         ret = converter.convert(params)
 
-        self.assertEqual(_from, ret['from'])
-        self.assertEqual(_to, ret['to'])
+        self.assertEqual(_from, str(ret['from']))
+        self.assertEqual(_to, str(ret['to']))
+
+        self.assertEqual(_bool, ret['bool'])
+        self.assertEqual(_bool2, ret['bool2'])
+
         self.assertEqual(_value, ret['value'])
-        self.assertEqual(_addresses, ret['addresses'])
+
+        self.assertEqual(_bool_array, ret['bool_array'])
+        self.assertEqual(_int_array, ret['int_array'])
+        self.assertEqual(_str_array1, ret['str_array1'])
+
+        self.assertEqual(_addresses[0], str(ret['addresses'][0]))
+        self.assertEqual(_addresses[1], str(ret['addresses'][1]))
+        self.assertEqual(_addresses[2], str(ret['addresses'][2]))
+        self.assertEqual(_addresses[3], str(ret['addresses'][3]))
+        self.assertEqual(_addresses[4], str(ret['addresses'][4]))
+
+        self.assertEqual(_value, ret['data']['value'])
+        self.assertEqual(_data, ret['data']['data'])
+
+        self.assertEqual(_unknown, ret['unknown'])
