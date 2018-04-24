fce4743166295e6da2c34385cf8609d9611b88d6
inwonKim <inwonkim@icon.foundation>
2018-04-24 15:43:22 +0900

    Changed the format of the values ​​of json.(#10)

--- iconservice/utils/json_util.py
+++ iconservice/utils/json_util.py
@@ -1,52 +1,91 @@
 from ..base.address import Address
 
+type_info = {
+    "type_table": {
+        "from": "address",
+        "to": "address",
+        "value": "int",
+        "values": "int[]",
+        "signature": "string",
+        "addresses": "address[]",
+        "success": "bool",
+        "boolList": "bool[]",
+        "stringList": "string[]"
+        # "balances": "dict[address:int]"
+    },
+    "two_depth_json_type": {
+        "address1": "address",
+        "address2": "address",
+        "value": "int",
+        "int_val": 'int',
+        "str_val": "string",
+        "data": {
+            "data-param1": "address",
+            "data-param2": "address",
+            "data-param3": "int",
+            "data-param4": 'int',
+            "data-param5": "string",
+            "data-param6": "address[]"
+        }
+    }
+}
+CONST_INT = "int"
+CONST_STRING = "string"
+CONST_BOOL = "bool"
+CONST_ADDRESS = "address"
+CONST_INT_ARRAY = "int[]"
+CONST_STRING_ARRAY = "string[]"
+CONST_BOOL_ARRAY = "bool[]"
+CONST_ADDRESS_ARRAY = "address[]"
 
-def convert_dict_values(json_dict: dict) -> dict:
+
+def convert_dict_values(json_dict: dict, method_name: str, *args) -> dict:
     """Convert json into appropriate format.
 
     :param json_dict:
+    :param method_name:
+    :param args:
     :return:
     """
     json_dictionary = {}
 
     for key in json_dict:
+        key_list = list(args)
+        key_list.append(key)
         if isinstance(json_dict[key], dict):
-            json_dictionary[key] = convert_dict_values(json_dict[key])
+            json_dictionary[key] = convert_dict_values(json_dict[key], method_name, *key_list)
         else:
-            json_dictionary[key] = convert_value(json_dict[key])
+            json_dictionary[key] = convert_value(json_dict[key], method_name, *key_list)
 
     return json_dictionary
 
 
-def convert_value(str_value_with_type: str):
+def convert_value(value: str, method_name: str, *keys):
     """Convert str value into specified type.
 
-    :param str_value_with_type:
-    example1: '123 -> int'
-    example2: '[1,2,3] -> int_array'
-    example3: '[True, False, False] -> bool_array'
+    :param value:
+    :param keys:
+    :param method_name:
     :return:
     """
-    specified_type = str_value_with_type.split(" -> ")[-1]
-    separator_index = str_value_with_type.rfind(" -> ")
-    value = str_value_with_type[:separator_index]
-    if specified_type == "int":
+    value_type = get_type_of_value(method_name, *keys)
+    if value_type == CONST_INT:
         return int(value, 0)
-    elif specified_type == "string":
+    elif value_type == CONST_STRING:
         return value
-    elif specified_type == "bool":
+    elif value_type == CONST_BOOL:
         return bool(value)
-    elif specified_type == "address":
+    elif value_type == CONST_ADDRESS:
         return Address(value[:2], bytes.fromhex(value[2:]))
-    elif specified_type == "int_array":
-        tmp_str_array = eval(value)
+    elif value_type == CONST_INT_ARRAY:
+        tmp_str_array = _convert_into_str_array(value)
         return [int(a, 0) for a in tmp_str_array]
-    elif specified_type == "bool_array":
+    elif value_type == CONST_BOOL_ARRAY:
         tmp_str_array = eval(value)
         return [string_to_bool(a) for a in tmp_str_array]
-    elif specified_type == "string_array":
+    elif value_type == CONST_STRING_ARRAY:
         return eval(value)
-    elif specified_type == "address_array":
+    elif value_type == CONST_ADDRESS_ARRAY:
         tmp_str_array = _convert_into_str_array(value)
         return [Address(a[:2], bytes.fromhex(a[2:])) for a in tmp_str_array]
     else:
@@ -66,9 +105,16 @@ def _convert_into_str_array(str_array: str) -> list:
 def string_to_bool(str_bool: str) -> bool:
     """Convert str_bool to bool value. This function will returns False When argument is 'False'
 
-    :param str_bool:
+    :param str_bool: ex) 'True', 'False'
     :return:
     """
     if bool(str_bool) is False or str_bool == "False":
         return False
     return True
+
+
+def get_type_of_value(method_name: str, *keys) -> str:
+    value_type = type_info[method_name]
+    for key in keys:
+        value_type = value_type[key]
+    return value_type
--- tests/test_json_utils.py
+++ tests/test_json_utils.py
@@ -6,26 +6,62 @@ from icon.iconservice.utils.json_util import *
 class TestJsonUtils(unittest.TestCase):
 
     def setUp(self):
-        self.one_depth_dict = {"address1": "hx1234abcd1234cdef1234abcd1234cdef1234cdef -> address",
-                          "address2": "cx1234abcd1234cdef1234abcd1234cdef1234cdef -> address",
-                          "value": "0x12 -> int",
-                          "int_val": "1234 -> int",
-                          "str_val": "abcdefg -> string"}
-        self.two_depth_dict = {"address1": "hx1234abcd1234cdef1234abcd1234cdef1234cdef -> address",
-                          "address2": "cx1234abcd1234cdef1234abcd1234cdef1234cdef -> address",
-                          "value": "0x1234abcd1234cdef1234abcd1234cdef1234cdef -> int",
-                          "int_val": '1234 -> int',
-                          "str_val": "abcdefg -> string",
+        self.type_table_dict = {
+            "from": "hx1234abcd1234cdef1234abcd1234cdef1234cdef",
+            "to": "cx1234abcd1234cdef1234abcd1234cdef1234cdef",
+            "value": "0x12",
+            "values": "[1,2,3,4,5,6,'10', '0x12']",
+            "signature": "sssssig",
+            "addresses": "['hx1234abcd1234cdef1234abcd1234cdef1234cdef', hx1234abcd1234cdef1234abcd1234cdef1234cdee']",
+            "success": "True",
+            "boolList" :"[True, False, 'True', 'False']",
+            "stringList": "['asdf', 'zxcv', 'qwer']"
+        }
+        self.two_depth_dict = {"address1": "hx1234abcd1234cdef1234abcd1234cdef1234cdef",
+                          "address2": "cx1234abcd1234cdef1234abcd1234cdef1234cdef",
+                          "value": "0x1234abcd1234cdef1234abcd1234cdef1234cdef",
+                          "int_val": '1234',
+                          "str_val": "abcdefg",
                           "data": {
-                                "data-param1": "hx1234abcd1234cdef1234abcd1234cdef1234cdef -> address",
-                                "data-param2": "cx1234abcd1234cdef1234abcd1234cdef1234cdef -> address",
-                                "data-param3": "0x1234 -> int",
-                                "data-param4": '1234 -> int',
-                                "data-param5": "abcdefghi -> string",
+                                "data-param1": "hx1234abcd1234cdef1234abcd1234cdef1234cdef",
+                                "data-param2": "cx1234abcd1234cdef1234abcd1234cdef1234cdef",
+                                "data-param3": "0x1234",
+                                "data-param4": '1234',
+                                "data-param5": "abcdefghi",
                                 "data-param6": "[hx1234abcd1234cdef1234abcd1234cdef1234cdef, \
-                                hx1234abcd1234cdef1234abcd1234cdef1234cdef] -> address_array"
+                                hx1234abcd1234cdef1234abcd1234cdef1234cdef]"
                             }
                           }
+        self.type_info = {
+            "type_table": {
+                "from": "address",
+                "to": "address",
+                "value": "int",
+                "values": "int[]",
+                "signature": "string",
+                "addresses": "address[]",
+                "success": "bool",
+                "boolList": "bool[]",
+                "stringList": "string[]"
+                # "balances": "dict[address:int]"
+                },
+            "two_depth_json_type": {
+                "address1": "address",
+                "address2": "address",
+                "value": "int",
+                "int_val": 'int',
+                "str_val": "string",
+                "data": {
+                    "data-param1": "address",
+                    "data-param2": "address",
+                    "data-param3": "int",
+                    "data-param4": 'int',
+                    "data-param5": "string",
+                    "data-param6": "address[]"
+                }
+            }
+        }
+
         self.eoa = "hx1234abcd1234cdef1234abcd1234cdef1234cdef"
         self.ca = "cx1234abcd1234cdef1234abcd1234cdef1234cdef"
         self.value = '0x12'
@@ -44,47 +80,55 @@ class TestJsonUtils(unittest.TestCase):
                               Address('hx', bytes.fromhex('1234abcd1234cdef1234abcd1234cdef1234cdee'))]
 
     def test_convert_value(self):
-        self.assertEqual(str, type(convert_value(self.string + " -> string")))
-        self.assertEqual(self.string, convert_value(self.string + " -> string"))
+        self.assertEqual(str, type(convert_value(self.string, "type_table", "signature")))
+        self.assertEqual(self.string, convert_value(self.string, "type_table", "signature"))
 
-        self.assertEqual(int, type(convert_value(self.value + " -> int")))
-        self.assertEqual(int(self.value, 0), convert_value(self.value + " -> int"))
+        self.assertEqual(int, type(convert_value(self.value, "type_table", "value")))
+        self.assertEqual(int(self.value, 0), convert_value(self.value, "type_table", "value"))
 
-        self.assertEqual(Address, type(convert_value(self.eoa + " -> address")))
-        self.assertEqual("hx", convert_value(self.eoa + " -> address").prefix)
-        self.assertEqual("1234abcd1234cdef1234abcd1234cdef1234cdef", convert_value(self.eoa + " -> address").body.hex())
+        self.assertEqual(Address, type(convert_value(self.eoa, "type_table", "from")))
+        self.assertEqual("hx", convert_value(self.eoa, "type_table", "from").prefix)
+        self.assertEqual("1234abcd1234cdef1234abcd1234cdef1234cdef"
+                         , convert_value(self.eoa, "type_table", "from").body.hex())
 
-        self.assertEqual(Address, type(convert_value(self.ca + " -> address")))
-        self.assertEqual("cx", convert_value(self.ca + " -> address").prefix)
-        self.assertEqual("1234abcd1234cdef1234abcd1234cdef1234cdef", convert_value(self.ca + " -> address").body.hex())
+        self.assertEqual(Address, type(convert_value(self.ca, "type_table", "to")))
+        self.assertEqual("cx", convert_value(self.ca, "type_table", "to").prefix)
+        self.assertEqual("1234abcd1234cdef1234abcd1234cdef1234cdef"
+                         , convert_value(self.ca, "type_table", "to").body.hex())
 
-        self.assertEqual(bool, type(convert_value(self.bool_value + " -> bool")))
-        self.assertEqual(True, convert_value(self.bool_value + " -> bool"))
+        self.assertEqual(bool, type(convert_value(self.bool_value, "type_table", "success")))
+        self.assertEqual(True, convert_value(self.bool_value, "type_table", "success"))
 
-        self.assertEqual(self.int_array, convert_value(self.int_array_str + " -> int_array"))
-        self.assertEqual(self.bool_array, convert_value(self.bool_array_str + " -> bool_array"))
+        self.assertEqual(self.int_array, convert_value(self.int_array_str, "type_table", "values"))
+        self.assertEqual(self.bool_array, convert_value(self.bool_array_str, "type_table", "boolList"))
 
         self.assertEqual(self.string_array
-                         , convert_value(self.string_array_str + " -> string_array"))
+                         , convert_value(self.string_array_str, "type_table", "stringList"))
 
-        self.assertTrue(self.address_array[0] == convert_value(self.address_array_str + " -> address_array")[0])
-        self.assertTrue(self.address_array[1] == convert_value(self.address_array_str + " -> address_array")[1])
+        self.assertTrue(self.address_array[0] == convert_value(self.address_array_str, "type_table", "addresses")[0])
+        self.assertTrue(self.address_array[1] == convert_value(self.address_array_str, "type_table", "addresses")[1])
 
     def test_convert_dict_values(self):
-        self.assertEqual(self.one_depth_dict.keys(), convert_dict_values(self.one_depth_dict).keys())
-        self.assertEqual(self.two_depth_dict.keys(), convert_dict_values(self.two_depth_dict).keys())
-        self.assertEqual(self.two_depth_dict["data"].keys(), convert_dict_values(self.two_depth_dict["data"]).keys())
+        self.assertEqual(self.type_table_dict.keys(), convert_dict_values(self.type_table_dict, "type_table").keys())
+        self.assertEqual(self.two_depth_dict.keys()
+                         , convert_dict_values(self.two_depth_dict, "two_depth_json_type").keys())
+        self.assertEqual(self.two_depth_dict["data"].keys(),
+                         convert_dict_values(self.two_depth_dict["data"], "two_depth_json_type", "data").keys())
 
-        self.assertEqual(Address, type(convert_dict_values(self.one_depth_dict)["address1"]))
-        self.assertEqual(Address, type(convert_dict_values(self.one_depth_dict)["address2"]))
-        self.assertEqual(Address, type(convert_dict_values(self.two_depth_dict["data"])["data-param1"]))
+        self.assertEqual(Address, type(convert_dict_values(self.type_table_dict, "type_table")["from"]))
+        self.assertEqual(Address, type(convert_dict_values(self.type_table_dict, "type_table")["to"]))
+        self.assertEqual(Address
+                         , type(convert_dict_values(self.two_depth_dict, "two_depth_json_type")["data"]["data-param1"]))
 
-        self.assertEqual(self.eoa, str(convert_dict_values(self.one_depth_dict)['address1']))
-        self.assertEqual(self.ca, str(convert_dict_values(self.one_depth_dict)['address2']))
+        self.assertEqual(self.eoa, str(convert_dict_values(self.type_table_dict, "type_table")['from']))
+        self.assertEqual(self.ca, str(convert_dict_values(self.type_table_dict, "type_table")['to']))
 
-        self.assertEqual(self.eoa, str(convert_dict_values(self.two_depth_dict)['data']['data-param1']))
-        self.assertEqual(self.ca, str(convert_dict_values(self.two_depth_dict)['data']['data-param2']))
-        self.assertEqual(int(self.value, 0), convert_dict_values(self.one_depth_dict)['value'])
+        self.assertEqual(self.eoa
+                         , str(convert_dict_values(self.two_depth_dict, "two_depth_json_type")['data']['data-param1']))
+        self.assertEqual(self.ca
+                         , str(convert_dict_values(self.two_depth_dict, "two_depth_json_type")['data']['data-param2']))
+        self.assertEqual(int(self.value, 0)
+                         , convert_dict_values(self.type_table_dict, "type_table")['value'])
 
 
 if __name__ == "__main__":
