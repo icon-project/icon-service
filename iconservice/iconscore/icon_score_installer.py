c213ff5836bd5ec8d5509919c15b85f9ece1aa91
inwonKim <inwonkim@icon.foundation>
2018-04-26 19:42:24 +0900

    Extract files from memory.(#11)

--- iconservice/iconscore/icon_score_installer.py
+++ iconservice/iconscore/icon_score_installer.py
@@ -13,14 +13,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import io
 import logging
 import os
 import zipfile
 import shutil
 
-from ..base.address import Address
-from ..base.exception import ScoreInstallException, ScoreInstallExtractException
-from ..base.exception import ScoreInstallWriteZipfileException
+from icon.iconservice.base.address import Address
+from icon.iconservice.base.exception import ScoreInstallException, ScoreInstallExtractException
+from icon.iconservice.base.exception import ScoreInstallWriteZipfileException
 
 CONST_SCORE_EXISTS_ERROR_CODE = 918
 CONST_WRITE_ZIPFILE_ERROR_CODE = 919
@@ -51,90 +52,102 @@ class IconScoreInstaller(object):
         score_id = str_block_height + "_" + str_transaction_index
         score_root_path = os.path.join(self.icon_score_root_path, str_address)
         install_path = os.path.join(score_root_path, score_id)
-        zip_path = install_path + ".zip"
+        # zip_path = install_path + ".zip"
         try:
-            if not os.path.exists(score_root_path):
-                os.makedirs(score_root_path)
-            if os.path.exists(install_path):
-                raise ScoreInstallException(f"You already have a SCORE of this version.")
-            zip_path = IconScoreInstaller.write_zipfile_with_bytes(zip_path, data)
-            zip_root_directory_name = IconScoreInstaller.extract_files(self.icon_score_root_path, zip_path)
+            if not os.path.exists(install_path):
+                os.makedirs(install_path)
 
-            IconScoreInstaller.remove_exists_archive(zip_path)
-            shutil.move(os.path.join(self.icon_score_root_path, zip_root_directory_name),
-                        install_path)
+            file_info_generator = IconScoreInstaller.extract_files_(data)
+            for name, file_info in file_info_generator:
+                print(name, file_info)
+                with file_info as file_info_context, open(os.path.join(install_path, name), 'wb') as dest:
+                    dest.write(file_info_context.read())
             return CONST_SUCCESS_INSTALL_SCORE
         except ScoreInstallException as e:
-            logging.debug(e.message)
+            print(e.message)
             return CONST_SCORE_EXISTS_ERROR_CODE
         except ScoreInstallWriteZipfileException:
             return CONST_WRITE_ZIPFILE_ERROR_CODE
         except ScoreInstallExtractException:
             return CONST_EXTRACT_FILES_ERROR_CODE
         except PermissionError as pe:
-            logging.debug(pe)
+            print(pe)
             return CONST_PERMISSION_ERROR_CODE
 
     @staticmethod
-    def extract_files(install_path: str, archive_path: str) -> str:
-        """Extract files from zip file.
+    def extract_files_(data: bytes):
+        with zipfile.ZipFile(io.BytesIO(data)) as memory_zip:
+            for zip_info in memory_zip.infolist():
+                with memory_zip.open(zip_info) as file:
+                    if zip_info.filename.find('__MACOSX') != -1:
+                        continue
+                    if zip_info.filename.find('/') == -1:
+                        yield zip_info.filename, file
+                    else:
+                        file_name_index = zip_info.filename.find('/')
+                        yield zip_info.filename, file
 
-        :param install_path: Path where score will be installed.
-        :param archive_path: The archive_path of SCORE.
-        :return:
-        Will return root directory of SCORE.
-        """
-        try:
-            if zipfile.is_zipfile(archive_path) is False:
-                IconScoreInstaller.remove_exists_archive(archive_path)
-                raise ScoreInstallExtractException("The data you sent is not a zip file.")
-            else:
-                with zipfile.ZipFile(archive_path, 'r') as z:
-                    file_name_list = z.namelist()
-                    file_name_prefix_list = [prefix.split('/')[0] for prefix in file_name_list
-                                             if not prefix.startswith('__MACOSX')]
-                    is_zipfile_covered = all(a == file_name_prefix_list[0] for a in file_name_prefix_list)
-                    zip_root_name = file_name_list[0].split("/")[0]
+    # @staticmethod
+    # def extract_files(install_path: str, archive_path: str) -> str:
+    #     """Extract files from zip file.
+    #
+    #     :param install_path: Path where score will be installed.
+    #     :param archive_path: The archive_path of SCORE.
+    #     :return:
+    #     Will return root directory of SCORE.
+    #     """
+    #     try:
+    #         if zipfile.is_zipfile(archive_path) is False:
+    #             IconScoreInstaller.remove_exists_archive(archive_path)
+    #             raise ScoreInstallExtractException("The data you sent is not a zip file.")
+    #         else:
+    #             with zipfile.ZipFile(archive_path, 'r') as z:
+    #                 file_name_list = z.namelist()
+    #                 file_name_prefix_list = [prefix.split('/')[0] for prefix in file_name_list
+    #                                          if not prefix.startswith('__MACOSX')]
+    #                 is_zipfile_covered = all(a == file_name_prefix_list[0] for a in file_name_prefix_list)
+    #                 zip_root_name = file_name_list[0].split("/")[0]
+    #
+    #                 if is_zipfile_covered:
+    #                     IconScoreInstaller._extract_files(file_name_list, install_path, z)
+    #                     return zip_root_name
+    #                 IconScoreInstaller._extract_files(file_name_list, install_path + '/temp', z)
+    #                 return 'temp'
+    #
+    #     except FileNotFoundError:
+    #
+    #         print(f"{archive_path} not found. check file path.")
+    #         raise ScoreInstallExtractException(f"{archive_path} not found. check file path.")
+    #     except IsADirectoryError:
+    #         print(f"{archive_path} is a directory. check file path.")
+    #         raise ScoreInstallExtractException(f"{archive_path} is a directory. check file path.")
+    #     except PermissionError:
+    #         raise ScoreInstallExtractException("Permission error")
+    #     except NotADirectoryError:
+    #         print(f"{install_path} is not a directory")
+    #         raise ScoreInstallExtractException(f"{install_path} is not a directory")
+    #     except zipfile.BadZipFile:
+    #         print(f"{archive_path} is bad zip file.")
+    #         raise ScoreInstallExtractException(f"{archive_path} is bad zip file.")
+    #     except zipfile.LargeZipFile:
+    #         print(f"{archive_path} is too Large Zipfile.")
+    #         raise ScoreInstallExtractException(f"{archive_path} is too Large file.")
+    #     except Exception as e:
+    #         print(e)
+    #         raise ScoreInstallExtractException("Exception occurred while extracting zip file.")
 
-                    if is_zipfile_covered:
-                        IconScoreInstaller._extract_files(file_name_list, install_path, z)
-                        return zip_root_name
-                    IconScoreInstaller._extract_files(file_name_list, install_path + '/temp', z)
-                    return 'temp'
-
-        except FileNotFoundError:
-            logging.debug(f"{archive_path} not found. check file path.")
-            raise ScoreInstallExtractException(f"{archive_path} not found. check file path.")
-        except IsADirectoryError:
-            logging.debug(f"{archive_path} is a directory. check file path.")
-            raise ScoreInstallExtractException(f"{archive_path} is a directory. check file path.")
-        except PermissionError:
-            raise ScoreInstallExtractException("Permission error")
-        except NotADirectoryError:
-            logging.debug(f"{install_path} is not a directory")
-            raise ScoreInstallExtractException(f"{install_path} is not a directory")
-        except zipfile.BadZipFile:
-            logging.debug(f"{archive_path} is bad zip file.")
-            raise ScoreInstallExtractException(f"{archive_path} is bad zip file.")
-        except zipfile.LargeZipFile:
-            logging.debug(f"{archive_path} is too Large Zipfile.")
-            raise ScoreInstallExtractException(f"{archive_path} is too Large file.")
-        except Exception as e:
-            logging.debug(e)
-            raise ScoreInstallExtractException("Exception occurred while extracting zip file.")
-
-    @staticmethod
-    def _extract_files(file_name_list: str, install_path: str, zip_file: object) -> None:
-        """Methods used in the IconScoreInstaller.extract_files.
-
-        :param file_name_list: List of file names inside the zip file.
-        :param install_path: Path where score will be installed.
-        :param zip_file: zipfile object.
-        :return:
-        """
-        for file_name in file_name_list:
-            if (not file_name.startswith("__MACOSX")) and file_name.find("__pycache__") == -1:
-                zip_file.extract(file_name, install_path)
+    # @staticmethod
+    # def _extract_files(file_name_list: str, install_path: str, zip_file: object) -> None:
+    #     """Methods used in the IconScoreInstaller.extract_files.
+    #
+    #     :param file_name_list: List of file names inside the zip file.
+    #     :param install_path: Path where score will be installed.
+    #     :param zip_file: zipfile object.
+    #     :return:
+    #     """
+    #     for file_name in file_name_list:
+    #         if (not file_name.startswith("__MACOSX")) and file_name.find("__pycache__") == -1:
+    #             zip_file.extract(file_name, install_path)
 
     @staticmethod
     def remove_exists_archive(archive_path: str) -> None:
@@ -157,17 +170,17 @@ class IconScoreInstaller(object):
         :return:
         """
         if os.path.isfile(archive_path):
-            logging.debug(f"{archive_path} file exists.")
+            print(f"{archive_path} file exists.")
             raise ScoreInstallWriteZipfileException(f"{archive_path} file exists.")
         try:
             with open(archive_path, 'wb') as f:
                 f.write(byte_data)
             return archive_path
         except IsADirectoryError:
-            logging.debug(f"{archive_path} is a directory. check file path.")
+            print(f"{archive_path} is a directory. check file path.")
             raise ScoreInstallWriteZipfileException(f"{archive_path} is a directory. check file path.")
         except PermissionError:
-            logging.debug(f"permission error")
+            print(f"permission error")
             print(archive_path)
             return ScoreInstallWriteZipfileException(f"permission error")
 
@@ -177,3 +190,14 @@ class IconScoreInstaller(object):
         with open(archive_path, 'rb') as f:
             byte_data = f.read()
             return byte_data
+
+
+def main():
+    installer = IconScoreInstaller('./')
+    address = Address.from_string('cx1234123412341234123412341234123412342134')
+    installer.install(address, IconScoreInstaller.read_zipfile_as_byte('test.zip'), 1234, 12)
+    # installer.install(address, IconScoreInstaller.read_zipfile_as_byte('icx.zip'), 124, 12)
+
+
+if __name__ == "__main__":
+    main()
