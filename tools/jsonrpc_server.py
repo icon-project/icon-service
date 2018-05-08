02575535eea1fd8fa914ac511295e71cb29f49d0
Jaechang Namgoong <jnamgoong@icon.foundation>
2018-05-08 18:42:47 +0900

    initial commit for #39

--- tools/jsonrpc_server.py
+++ tools/jsonrpc_server.py
@@ -12,25 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import _ssl
+# import _ssl
 import json
 import logging
-import ssl
-import threading
+# import ssl
+# import threading
 
 from flask import Flask, request, Response
 from flask_restful import reqparse, Api
 from jsonrpcserver import methods
 
-from icon.tools.score_invoker import ScoreInvoker
-from loopchain import utils, configure as conf, util
-from loopchain.baseservice import CommonThread
-from loopchain.protos import message_code
-from loopchain.rest_server import IcnTxPrevalidator
+# from icon.tools.score_invoker import ScoreInvoker
+# from loopchain import utils, configure as conf, util
+# from loopchain.baseservice import CommonThread
+# from loopchain.protos import message_code
+# from loopchain.rest_server import IcnTxPrevalidator
 
 
 class MockDispatcher:
-    tx_validator = IcnTxPrevalidator()
+    # tx_validator = IcnTxPrevalidator()
 
     @staticmethod
     def dispatch():
@@ -44,27 +44,29 @@ class MockDispatcher:
     @staticmethod
     @methods.add
     def icx_sendTransaction(**kwargs):
-        util.logger.spam(f"icx_sendTransaction{kwargs}")
-        tx = MockDispatcher.tx_validator.validate_dumped_tx(json.dumps(kwargs), conf.LOOPCHAIN_DEFAULT_CHANNEL)
-        if tx is None:
-            return {"response_code": message_code.Response.fail_create_tx,
-                    "message": f"tx validate fail. tx data :: {kwargs}"}
+        logging.debug(f"icx_sendTransaction{kwargs}")
+        # tx = MockDispatcher.tx_validator.validate_dumped_tx(json.dumps(kwargs), conf.LOOPCHAIN_DEFAULT_CHANNEL)
+        # if tx is None:
+        #     return {"response_code": message_code.Response.fail_create_tx,
+        #             "message": f"tx validate fail. tx data :: {kwargs}"}
 
-        # util.logger.spam("********************************************")
-        # util.logger.spam(f"{tx.json_dumps()}")
-        # util.logger.spam("********************************************")
+        tx = json.dumps(kwargs)
+
+        # logging.debug("********************************************")
+        # logging.debug(f"{tx.json_dumps()}")
+        # logging.debug("********************************************")
 
         # deliver the tx object to IconScoreService directly here!
-        res = ScoreInvoker().send_transaction(tx)
+        # res = ScoreInvoker().send_transaction(tx)
 
-        response_data = {'response_code': res.code}
-        if res.code != message_code.Response.success:
-            response_data['message'] = res.message
-        else:
-            response_data['tx_hash'] = res.message
+        # response_data = {'response_code': res.code}
+        # if res.code != message_code.Response.success:
+        #     response_data['message'] = res.message
+        # else:
+        #     response_data['tx_hash'] = res.message
 
-        util.logger.spam(f"response_data: ({response_data})")
-        return response_data
+        # util.logger.spam(f"response_data: ({response_data})")
+        return tx
 
     @staticmethod
     @methods.add
@@ -126,21 +128,21 @@ class FlaskServer():
         self.__parser = reqparse.RequestParser()
 
         # SSL 적용 여부에 따라 context 생성 여부를 결정한다.
-        if conf.REST_SSL_TYPE == conf.SSLAuthType.none:
-            self.__ssl_context = None
-        elif conf.REST_SSL_TYPE == conf.SSLAuthType.server_only:
-            self.__ssl_context = (conf.DEFAULT_SSL_CERT_PATH, conf.DEFAULT_SSL_KEY_PATH)
-        elif conf.REST_SSL_TYPE == conf.SSLAuthType.mutual:
-            self.__ssl_context = ssl.SSLContext(_ssl.PROTOCOL_SSLv23)
-
-            self.__ssl_context.verify_mode = ssl.CERT_REQUIRED
-            self.__ssl_context.check_hostname = False
-
-            self.__ssl_context.load_verify_locations(cafile=conf.DEFAULT_SSL_TRUST_CERT_PATH)
-            self.__ssl_context.load_cert_chain(conf.DEFAULT_SSL_CERT_PATH, conf.DEFAULT_SSL_KEY_PATH)
-        else:
-            utils.exit_and_msg(
-                f"REST_SSL_TYPE must be one of [0,1,2]. But now conf.REST_SSL_TYPE is {conf.REST_SSL_TYPE}")
+        # if conf.REST_SSL_TYPE == conf.SSLAuthType.none:
+        #     self.__ssl_context = None
+        # elif conf.REST_SSL_TYPE == conf.SSLAuthType.server_only:
+        #     self.__ssl_context = (conf.DEFAULT_SSL_CERT_PATH, conf.DEFAULT_SSL_KEY_PATH)
+        # elif conf.REST_SSL_TYPE == conf.SSLAuthType.mutual:
+        #     self.__ssl_context = ssl.SSLContext(_ssl.PROTOCOL_SSLv23)
+        #
+        #     self.__ssl_context.verify_mode = ssl.CERT_REQUIRED
+        #     self.__ssl_context.check_hostname = False
+        #
+        #     self.__ssl_context.load_verify_locations(cafile=conf.DEFAULT_SSL_TRUST_CERT_PATH)
+        #     self.__ssl_context.load_cert_chain(conf.DEFAULT_SSL_CERT_PATH, conf.DEFAULT_SSL_KEY_PATH)
+        # else:
+        #     utils.exit_and_msg(
+        #         f"REST_SSL_TYPE must be one of [0,1,2]. But now conf.REST_SSL_TYPE is {conf.REST_SSL_TYPE}")
 
     @property
     def app(self):
@@ -158,28 +160,28 @@ class FlaskServer():
         self.__app.add_url_rule('/api/v2', view_func=MockDispatcher.dispatch, methods=['POST'])
 
 
-class SimpleRestServer(CommonThread):
+class SimpleRestServer():
     def __init__(self, peer_port, peer_ip_address=None):
-        if peer_ip_address is None:
-            peer_ip_address = conf.IP_LOCAL
-        CommonThread.__init__(self)
+        # if peer_ip_address is None:
+        #     peer_ip_address = conf.IP_LOCAL
+        # CommonThread.__init__(self)
         self.__peer_port = peer_port
         self.__peer_ip_address = peer_ip_address
 
         self.__server = FlaskServer()
         self.__server.set_resource()
 
-    def run(self, event: threading.Event):
-        ScoreInvoker().init_stub(self.__peer_ip_address, self.__peer_port)
-        ScoreInvoker().score_load()
-        api_port = self.__peer_port + conf.PORT_DIFF_REST_SERVICE_CONTAINER
-        logging.debug("SimpleRestServer run... %s", str(api_port))
+    def run(self):
+        # ScoreInvoker().init_stub(self.__peer_ip_address, self.__peer_port)
+        # ScoreInvoker().score_load()
+        api_port = self.__peer_port + 1900 #conf.PORT_DIFF_REST_SERVICE_CONTAINER
+        logging.error("SimpleRestServer run... %s", str(api_port))
 
-        event.set()
+        # event.set()
         self.__server.app.run(port=api_port, host=self.__peer_ip_address,
-                              debug=False, ssl_context=self.__server.ssl_context)
+                              debug=False)
 
 
 if __name__ == '__main__':
     server = SimpleRestServer(7100)
-    server.start()
+    server.run()
