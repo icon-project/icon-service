898e7aa098292b97dea06ee0c30587c36a1222a9
Yongwoo Lee <whylee259@gmail.com>
2018-06-08 22:50:27 +0900

    Add `tbears_jsonrpc_api_v3.md` to deploy list

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ export  AWS_ACCESS_KEY_ID=AKIAJYKHNVJS4GYQTV2Q
 export AWS_SECRET_ACCESS_KEY=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
 
 mkdir $1
-cp dist/*$1*.whl docs/CHANGELOG.md docs/dapp_guide.md docs/jsonrpc_api.md $1
+cp dist/*$1*.whl docs/CHANGELOG.md docs/dapp_guide.md docs/tbears_jsonrpc_api_v3.md $1
 aws s3 cp $1 s3://unchain.icon.foundation/$1 --recursive --acl public-read
 rm -rf $1
