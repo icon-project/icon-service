515eacfa2e8b4ea55502526b1f0242e9f043bf03
leeheonseung <mycom22@icon.Foundation>
2018-07-02 19:55:53 +0900

    reimplement typeconverter (#159)

--- iconservice/base/type_converter.py
+++ iconservice/base/type_converter.py
@@ -15,116 +15,184 @@
 # limitations under the License.
 
 from copy import deepcopy
-from typing import Any, get_type_hints
-from ..base.address import Address
-from ..utils import int_to_bytes
+from typing import Union, Any, get_type_hints
+from enum import IntEnum
 
+from .address import Address
+from .exception import InvalidParamsException
 
 score_base_support_type = (int, str, bytes, bool, Address)
 
 
-class TypeConverter(object):
+class ParamType(IntEnum):
+    block = 0
+    transaction = 1
 
-    CONST_INT = "int"
-    CONST_STRING = "string"
-    CONST_BOOL = "bool"
-    CONST_ADDRESS = "address"
-    CONST_INT_ARRAY = "int[]"
-    CONST_STRING_ARRAY = "string[]"
-    CONST_BOOL_ARRAY = "bool[]"
-    CONST_ADDRESS_ARRAY = "address[]"
-    CONST_BYTES = "bytes"
+    invoke = 2
+    query = 3
 
-    def __init__(self, type_table: dict=None) -> None:
-        self.type_table = type_table
+    icx_call = 4
+    icx_get_balance = 5
+    icx_get_total_supply = 6
+    icx_get_score_api = 7
 
-    def convert(self, params: Any, recursive: bool) -> Any:
-        if isinstance(params, dict):
-            return self.convert_dict_values(params, recursive)
-        elif isinstance(params, list):
-            return self.convert_list_values(params, recursive)
-        return params
+    write_precommit = 8
+    remove_precommit = 9
 
-    def convert_dict_values(self, input_params: dict, recursive: bool) -> dict:
-        """Convert json into appropriate format.
+    validate_transaction = 10
+    account_data = 11
+    genesis_data = 12
 
-        Original input is preserved after convert is done.
 
-        :param input_params:
-        :param recursive:
-        :return:
-        """
-        output = {}
+class ValueType(IntEnum):
+    NONE = 0
+    INT = 1
+    STRING = 2
+    BOOL = 3
+    ADDRESS = 4
+    BYTES = 5
 
-        for key, value in input_params.items():
-            if isinstance(value, dict):
-                if recursive:
-                    output[key] = self.convert_dict_values(value, recursive)
-                else:
-                    output[key] = deepcopy(value)
-            elif isinstance(value, list):
-                if recursive:
-                    output[key] = self.convert_list_values(value, recursive)
-                else:
-                    output[key] = deepcopy(value)
-            else:
-                output[key] = self.convert_value(key, value)
 
-        return output
+type_convert_templates = dict()
+CONVERT_USING_METHOD = 'CONVERT_USING_METHOD'
+SWITCH_KEY = "SWITCH_KEY"
 
-    def convert_list_values(self, input_params: list, recursive: bool) -> list:
-        output = []
 
-        for item in input_params:
-            if isinstance(item, dict):
-                item = self.convert_dict_values(item, recursive)
-            if isinstance(item, list) and recursive:
-                item = self.convert_list_values(item, recursive)
+class TypeConverter:
+    @staticmethod
+    def convert(params: dict, param_type: ParamType) -> Any:
+        if param_type is None:
+            return params
 
-            output.append(item)
-
-        return output
-
-    def convert_value(self, key: str, value: object) -> object:
-        """Convert str value into the type specified in _type_table
-
-        :param key:
-        :param value:
-        :return:
-        """
-        try:
-            if not isinstance(value, str):
-                return value
-
-            value_type = self.type_table.get(key)
-
-            if value_type == TypeConverter.CONST_INT:
-                return int(str(value), 0)
-            elif value_type == TypeConverter.CONST_BOOL:
-                return value == "True" or value is True
-            elif value_type == TypeConverter.CONST_ADDRESS:
-                return Address.from_string(value)
-            elif value_type == TypeConverter.CONST_ADDRESS_ARRAY:
-                return [Address.from_string(a) for a in value]
-            elif value_type == TypeConverter.CONST_BYTES:
-                return bytes.fromhex(value[2:])
-        except KeyError:
-            if isinstance(value, list):
-                return self.convert_list_values(value, recursive=True)
-        except:
-            pass
-
-        return value
+        copied_params = deepcopy(params)  # to avoid corrupting original data
+        converted_params = TypeConverter._convert(copied_params, type_convert_templates[param_type])
+        return converted_params
 
     @staticmethod
-    def make_annotations_from_method(func: callable):
+    def _convert(params: dict, template: Union[list, dict]) -> Any:
+        if not params or not template:
+            return params
+
+        if isinstance(params, dict) and isinstance(template, dict):
+            new_params = dict()
+            for key, value in params.items():
+                if TypeConverter._check_convert_using_method(key, template):
+                    ref_key_table = deepcopy(new_params)
+                    target_template = TypeConverter._get_convert_using_method_template(key, template)
+                    new_value = TypeConverter._convert_using_switch(value, ref_key_table, target_template)
+                else:
+                    new_value = TypeConverter._convert(value, template.get(key))
+                new_params[key] = new_value
+        elif isinstance(params, list) and isinstance(template, list):
+            new_params = list()
+            for item in params:
+                new_item = TypeConverter._convert(item, template[0])
+                new_params.append(new_item)
+        elif isinstance(template, ValueType):
+            new_params = TypeConverter._convert_value(params, template)
+        else:
+            new_params = params
+
+        return new_params
+
+    @staticmethod
+    def _check_convert_using_method(param: str, template: dict) -> bool:
+        params = template.get(param)
+        if isinstance(params, dict):
+            return CONVERT_USING_METHOD in params
+        return False
+
+    @staticmethod
+    def _get_convert_using_method_template(key: str, template: dict) -> dict:
+        tmp_params = template.get(key)
+        return tmp_params.get(CONVERT_USING_METHOD)
+
+    @staticmethod
+    def _convert_using_switch(params: dict, tmp_params: dict, template: Union[list, dict]) -> Any:
+        if not params or not template:
+            return params
+
+        switch_key = template.get(SWITCH_KEY)
+        templete_key = tmp_params.get(switch_key)
+        target_templete = template.get(templete_key)
+
+        if isinstance(params, dict) and isinstance(target_templete, dict):
+            new_params = dict()
+            for key, value in params.items():
+                new_value = TypeConverter._convert(value, target_templete.get(key))
+                new_params[key] = new_value
+        elif isinstance(params, list) and isinstance(target_templete, list):
+            new_params = list()
+            for item in params:
+                new_item = TypeConverter._convert(item, target_templete[0])
+                new_params.append(new_item)
+        elif isinstance(target_templete, ValueType):
+            new_params = TypeConverter._convert_value(params, target_templete)
+        else:
+            new_params = params
+
+        return new_params
+
+    @staticmethod
+    def _convert_value(value: Any, value_type: ValueType) -> Any:
+        if value_type == ValueType.INT:
+            converted_value = TypeConverter._convert_value_int(value)
+        elif value_type == ValueType.STRING:
+            converted_value = TypeConverter._convert_value_string(value)
+        elif value_type == ValueType.BOOL:
+            converted_value = TypeConverter._convert_value_bool(value)
+        elif value_type == ValueType.ADDRESS:
+            converted_value = TypeConverter._convert_value_address(value)
+        elif value_type == ValueType.BYTES:  # hash...(block_hash, tx_hash)
+            converted_value = TypeConverter._convert_value_bytes(value)
+        else:
+            converted_value = value
+        return converted_value
+
+    @staticmethod
+    def _convert_value_int(value: str) -> int:
+        if isinstance(value, str):
+            return int(value, 16)
+        else:
+            raise InvalidParamsException(f'TypeConvert Exception int value :{value}, type: {type(value)}')
+
+    @staticmethod
+    def _convert_value_string(value: str) -> str:
+        if isinstance(value, str):
+            return value
+        else:
+            raise InvalidParamsException(f'TypeConvert Exception str value :{value}, type: {type(value)}')
+
+    @staticmethod
+    def _convert_value_bool(value: str) -> bool:
+        if isinstance(value, str):
+            return bool(TypeConverter._convert_value_int(value))
+        else:
+            raise InvalidParamsException(f'TypeConvert Exception bool value :{value}, type: {type(value)}')
+
+    @staticmethod
+    def _convert_value_address(value: str) -> 'Address':
+        if isinstance(value, str):
+            return Address.from_string(value)
+        else:
+            raise InvalidParamsException(f'TypeConvert Exception address value :{value}, type: {type(value)}')
+
+    @staticmethod
+    def _convert_value_bytes(value: str) -> bytes:
+        if isinstance(value, str):
+            return bytes.fromhex(value)
+        else:
+            raise InvalidParamsException(f'TypeConvert Exception bytes value :{value}, type: {type(value)}')
+
+    @staticmethod
+    def make_annotations_from_method(func: callable) -> dict:
         hints = get_type_hints(func)
         if hints.get('return') is not None:
             del hints['return']
         return hints
 
     @staticmethod
-    def convert_params(annotation_params: dict, kw_params: dict) -> None:
+    def convert_data_params(annotation_params: dict, kw_params: dict) -> None:
 
         for key, param in annotation_params.items():
             if key == 'self' or key == 'cls':
@@ -134,30 +202,99 @@ class TypeConverter(object):
             if kw_param is None:
                 continue
 
-            kw_param = TypeConverter.__convert_value(param, kw_param)
+            kw_param = TypeConverter._convert_data_value(param, kw_param)
             kw_params[key] = kw_param
 
     @staticmethod
-    def __convert_value(annotation_type: type, param: Any):
+    def _convert_data_value(annotation_type: type, param: Any) -> Any:
         if annotation_type == int:
-            param = int(str(param), 0)
+            param = TypeConverter._convert_value_int(param)
         elif annotation_type == bool:
-            param = param == "True" or param is True
+            param = TypeConverter._convert_value_bool(param)
         elif annotation_type == Address:
-            if isinstance(param, Address):
-                param = param
-            else:
-                param = Address.from_string(param)
+            param = TypeConverter._convert_value_address(param)
         elif annotation_type == bytes:
-            if isinstance(param, int):
-                param = int_to_bytes(param)
-            elif isinstance(param, str):
-                param = param.encode()
-            elif isinstance(param, bool):
-                param = int(param)
-                param = int_to_bytes(param)
-            elif isinstance(param, Address):
-                byte_array = bytearray(param.body)
-                byte_array.append(param.prefix)
-                param = bytes(byte_array)
+            param = TypeConverter._convert_value_bytes(param)
         return param
+
+
+type_convert_templates[ParamType.block] = {
+    "blockHeight": ValueType.INT,
+    "blockHash": ValueType.BYTES,
+    "timestamp": ValueType.INT,
+    "prevBlockHash": ValueType.BYTES,
+}
+type_convert_templates[ParamType.transaction] = {
+    "method": ValueType.STRING,
+    "params": {
+        "txHash": ValueType.BYTES,
+        "version": ValueType.INT,
+        "from": ValueType.ADDRESS,
+        "to": ValueType.ADDRESS,
+        "value": ValueType.INT,
+        "stepLimit": ValueType.INT,
+        "timestamp": ValueType.INT,
+        "nonce": ValueType.INT,
+        "signature": ValueType.NONE,
+        "dataType": ValueType.STRING,
+        "data": ValueType.NONE
+    },
+    "genesisData": ValueType.NONE
+}
+
+type_convert_templates[ParamType.invoke] = {
+    "block": type_convert_templates[ParamType.block],
+    "transactions": [
+        type_convert_templates[ParamType.transaction]
+    ]
+}
+
+type_convert_templates[ParamType.icx_call] = {
+    "version": ValueType.INT,
+    "from": ValueType.ADDRESS,
+    "to": ValueType.ADDRESS,
+    "dataType": ValueType.STRING,
+    "data": ValueType.NONE
+}
+type_convert_templates[ParamType.icx_get_balance] = {
+    "version": ValueType.INT,
+    "address": ValueType.ADDRESS
+}
+type_convert_templates[ParamType.icx_get_total_supply] = {
+    "version": ValueType.INT
+}
+type_convert_templates[ParamType.icx_get_score_api] = type_convert_templates[ParamType.icx_get_balance]
+
+type_convert_templates[ParamType.query] = {
+    "method": ValueType.STRING,
+    "params": {
+        CONVERT_USING_METHOD: {
+            SWITCH_KEY: "method",
+            "icx_call": type_convert_templates[ParamType.icx_call],
+            "icx_getBalance": type_convert_templates[ParamType.icx_get_balance],
+            "icx_getTotalSupply": type_convert_templates[ParamType.icx_get_total_supply],
+            "icx_getScoreApi": type_convert_templates[ParamType.icx_get_score_api],
+        }
+    }
+}
+
+type_convert_templates[ParamType.write_precommit] = {
+    "blockHeight": ValueType.INT,
+    "blockHash": ValueType.BYTES
+}
+type_convert_templates[ParamType.remove_precommit] = type_convert_templates[ParamType.write_precommit]
+
+type_convert_templates[ParamType.validate_transaction] = type_convert_templates[ParamType.transaction]
+
+type_convert_templates[ParamType.account_data] = {
+    "name": ValueType.STRING,
+    "address": ValueType.ADDRESS,
+    "balance": ValueType.INT
+}
+
+type_convert_templates[ParamType.genesis_data] = {
+    "accounts": [
+        type_convert_templates[ParamType.account_data]
+    ],
+    "message": ValueType.STRING
+}
