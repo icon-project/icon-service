0f32251efa81973cdf41e1809ccb83f0dd850f33
Jaechang Namgoong <jnamgoong@icon.foundation>
2018-07-16 22:52:46 +0900

    Add getStepCosts and setStepCost

--- iconservice/builtin_scores/governance/governance.py
+++ iconservice/builtin_scores/governance/governance.py
@@ -15,12 +15,30 @@ STATUS_ACTIVE = 'active'
 STATUS_INACTIVE = 'inactive'
 STATUS_REJECTED = 'rejected'
 
+STEP_TYPE_DEFAULT = 'default'
+STEP_TYPE_CONTRACT_CALL = 'contractCall'
+STEP_TYPE_CONTRACT_CREATE = 'contractCreate'
+STEP_TYPE_CONTRACT_UPDATE = 'contractUpdate'
+STEP_TYPE_CONTRACT_DESTRUCT = 'contractDestruct'
+STEP_TYPE_CONTRACT_SET = 'contractSet'
+STEP_TYPE_SET = 'set'
+STEP_TYPE_REPLACE = 'replace'
+STEP_TYPE_DELETE = 'delete'
+STEP_TYPE_INPUT = 'input'
+STEP_TYPE_EVENT_LOG = 'eventLog'
+VALID_STEP_COST_KEYS = [STEP_TYPE_DEFAULT,
+                        STEP_TYPE_CONTRACT_CALL, STEP_TYPE_CONTRACT_CREATE, STEP_TYPE_CONTRACT_UPDATE,
+                        STEP_TYPE_CONTRACT_DESTRUCT, STEP_TYPE_CONTRACT_SET,
+                        STEP_TYPE_SET, STEP_TYPE_REPLACE, STEP_TYPE_DELETE, STEP_TYPE_INPUT,
+                        STEP_TYPE_EVENT_LOG]
+
 
 class Governance(IconScoreBase):
 
     _SCORE_STATUS = 'score_status'
     _AUDITOR_LIST = 'auditor_list'
     _STEP_PRICE = 'step_price'
+    _STEP_COSTS = 'step_costs'
 
     @eventlog(indexed=1)
     def Accepted(self, tx_hash: str):
@@ -34,18 +52,26 @@ class Governance(IconScoreBase):
     def StepPriceChanged(self, step_price: int):
         pass
 
+    @eventlog(indexed=1)
+    def StepCostChanged(self, step_type: str, cost: int):
+        pass
+
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
         self._score_status = DictDB(self._SCORE_STATUS, db, value_type=bytes, depth=3)
         self._auditor_list = ArrayDB(self._AUDITOR_LIST, db, value_type=Address)
         self._step_price = VarDB(self._STEP_PRICE, db, value_type=int)
+        self._step_costs = DictDB(self._STEP_COSTS, db, value_type=int)
 
     def on_install(self, stepPrice: int = 10 ** 12) -> None:
         super().on_install()
         # add owner into initial auditor list
+        Logger.debug(f'on_install: owner = "{self.owner}"', TAG)
         self._auditor_list.put(self.owner)
         # set initial step price
         self._step_price.set(stepPrice)
+        # set initial step costs
+        self._set_initial_step_costs()
 
     def on_update(self) -> None:
         super().on_update()
@@ -214,3 +240,42 @@ class Governance(IconScoreBase):
         Logger.debug(f'{header}: list len = {len(self._auditor_list)}', TAG)
         for auditor in self._auditor_list:
             Logger.debug(f' --- {auditor}', TAG)
+
+    def _set_initial_step_costs(self):
+        initial_costs = {
+            STEP_TYPE_DEFAULT: 4000,
+            STEP_TYPE_CONTRACT_CALL: 1500,
+            STEP_TYPE_CONTRACT_CREATE: 20000,
+            STEP_TYPE_CONTRACT_UPDATE: 8000,
+            STEP_TYPE_CONTRACT_DESTRUCT: -7000,
+            STEP_TYPE_CONTRACT_SET: 1000,
+            STEP_TYPE_SET: 20,
+            STEP_TYPE_REPLACE: 5,
+            STEP_TYPE_DELETE: -15,
+            STEP_TYPE_INPUT: 20,
+            STEP_TYPE_EVENT_LOG: 10
+        }
+        for key in VALID_STEP_COST_KEYS:
+            value: int = initial_costs[key]
+            self._step_costs[key] = value
+
+    @external(readonly=True)
+    def getStepCosts(self) -> dict:
+        result = {}
+        for key in VALID_STEP_COST_KEYS:
+            result[key] = self._step_costs[key]
+        return result
+
+    @external
+    def setStepCost(self, stepType: str, cost: int):
+        # only owner can set new step cost
+        if self.msg.sender != self.owner:
+            self.revert('Invalid sender: not owner')
+        if stepType not in VALID_STEP_COST_KEYS:
+            self.revert(f'Invalid step type: {stepType}')
+        if cost < 0:
+            if stepType != STEP_TYPE_CONTRACT_DESTRUCT and \
+                    stepType != STEP_TYPE_DELETE:
+                self.revert(f'Invalid step cost: {stepType}, {cost}')
+        self._step_costs[stepType] = cost
+        self.StepCostChanged(stepType, cost)
