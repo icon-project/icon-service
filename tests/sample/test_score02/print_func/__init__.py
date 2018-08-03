058327eb4dee03e846be1af926ccf77404c484cc
leeheonseung <mycom22@icon.Foundation>
2018-08-03 11:24:06 +0900

    fix unittest (#233)

---
+++ tests/sample/test_score02/print_func/__init__.py
@@ -0,0 +1,1 @@
+from .print_func import func_test
--- tests/test_icon_score_loader.py
+++ tests/test_icon_score_loader.py
@@ -15,55 +15,85 @@
 # limitations under the License.
 
 
+import inspect
 import unittest
-import os
-from iconservice.iconscore.icon_score_base import IconScoreBase
-from iconservice.iconscore.icon_score_loader import IconScoreLoader
+from os import path, makedirs, symlink
+from typing import TYPE_CHECKING
+
+from iconservice.base.address import AddressPrefix
 from iconservice.deploy.icon_score_deployer import IconScoreDeployer
 from iconservice.deploy import make_score_id
-from iconservice.base.address import AddressPrefix
-import inspect
+from iconservice.iconscore.icon_score_base import IconScoreBase
+from iconservice.iconscore.icon_score_context import ContextContainer, \
+    IconScoreContextFactory, IconScoreContextType
+from iconservice.iconscore.icon_score_context import IconScoreContext
+from iconservice.iconscore.icon_score_loader import IconScoreLoader
 from tests import create_address
+from tests.mock_db import create_mock_icon_score_db
 
-TEST_ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+if TYPE_CHECKING:
+    from iconservice.base.address import Address
+
+TEST_ROOT_PATH = path.abspath(path.join(path.dirname(__file__), '../'))
+
+
+class TestContextContainer(ContextContainer):
+    pass
+
+
+class MockIconScoreManager(object):
+    def get_owner(self, context, address):
+        return None
 
 
 class TestIconScoreLoader(unittest.TestCase):
-    _ROOT_SCORE_PATH = os.path.join(TEST_ROOT_PATH, 'score')
+    _ROOT_SCORE_PATH = 'tests/score'
+    _TEST_DB_PATH = 'tests/test_db'
 
     def setUp(self):
-        self._loader = IconScoreLoader(self._ROOT_SCORE_PATH)
-        self._address = create_address(AddressPrefix.CONTRACT, b'SampleToken')
-        print(self._address)
-        archive_path = 'tests/sample/valid.zip'
-        archive_path = os.path.join(TEST_ROOT_PATH, archive_path)
-        zip_bytes = self.read_zipfile_as_byte(archive_path)
-        install_path = os.path.join(TEST_ROOT_PATH, self._ROOT_SCORE_PATH, self._address.to_bytes().hex(), '0_0')
-        self.__unpack_zip_file(install_path, zip_bytes)
+        self._score_path = path.join(TEST_ROOT_PATH, self._ROOT_SCORE_PATH)
+        self._loader = IconScoreLoader(self._score_path)
+        self._addr_test_score01 = create_address(AddressPrefix.CONTRACT, b'test_score01')
+        self._addr_test_score02 = create_address(AddressPrefix.CONTRACT, b'test_score02')
+
+        self.db = create_mock_icon_score_db()
+        self._factory = IconScoreContextFactory(max_size=1)
+        IconScoreContext.icon_score_manager = MockIconScoreManager()
+        self._context = self._factory.create(IconScoreContextType.DIRECT)
+        self._context_container = TestContextContainer()
+        self._context_container._put_context(self._context)
 
     def tearDown(self):
-        remove_path = os.path.join(TEST_ROOT_PATH, self._ROOT_SCORE_PATH)
+        remove_path = path.join(TEST_ROOT_PATH, self._ROOT_SCORE_PATH)
+        IconScoreDeployer.remove_existing_score(remove_path)
+        remove_path = path.join(TEST_ROOT_PATH, self._TEST_DB_PATH)
         IconScoreDeployer.remove_existing_score(remove_path)
         pass
 
     @staticmethod
-    def read_zipfile_as_byte(archive_path: str) -> bytes:
-        with open(archive_path, 'rb') as f:
-            byte_data = f.read()
-            return byte_data
+    def __ensure_dir(dir_path):
+        if not path.exists(dir_path):
+            makedirs(dir_path)
 
-    @staticmethod
-    def __unpack_zip_file(install_path: str, data: bytes):
-        file_info_generator = IconScoreDeployer.extract_files_gen(data)
-        for name, file_info, parent_directory in file_info_generator:
-            if not os.path.exists(os.path.join(install_path, parent_directory)):
-                os.makedirs(os.path.join(install_path, parent_directory))
-            with file_info as file_info_context, open(os.path.join(install_path, name), 'wb') as dest:
-                contents = file_info_context.read()
-                dest.write(contents)
-        return True
+    def load_proj(self, proj: str, addr_score: 'Address') -> callable:
+        target_path = path.join(self._score_path, addr_score.to_bytes().hex())
+        makedirs(target_path, exist_ok=True)
+        score_id = make_score_id(0, 0)
+        target_path = path.join(target_path, score_id)
+
+        ref_path = path.join(TEST_ROOT_PATH, 'tests/sample/{}'.format(proj))
+        symlink(ref_path, target_path, target_is_directory=True)
+        return self._loader.load_score(addr_score.to_bytes().hex(), score_id)
 
     def test_install(self):
-        score_id = make_score_id(0, 0)
-        score = self._loader.load_score(self._address.to_bytes().hex(), score_id)
+        self.__ensure_dir(self._score_path)
+
+        score = self.load_proj('test_score01', self._addr_test_score01)
+        print('test_score01', score.get_api())
+        score = self.load_proj('test_score02', self._addr_test_score02)
+        print('test_score02', score.get_api())
+
+        ins_score = score(self.db)
+
+        ins_score.print_test()
         self.assertTrue(IconScoreBase in inspect.getmro(score))
