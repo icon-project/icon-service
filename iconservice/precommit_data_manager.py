6c2de02b28156ca0c88922423b7b0a05211a6752
Chiwon Cho <goldworm@icon.foundation>
2018-08-09 07:40:54 +0900

    Use the block in PrecommitData instead of block info sent by loopchain on commit

--- iconservice/precommit_data_manager.py
+++ iconservice/precommit_data_manager.py
@@ -37,6 +37,7 @@ class PrecommitData(object):
         self.block_batch = block_batch
         self.block_result = block_result
         self.score_mapper = score_mapper
+        self.block = block_batch.block
 
 
 class PrecommitDataManager(object):
@@ -118,6 +119,8 @@ class PrecommitDataManager(object):
 
         if self._last_block is None:
             return
+
+        precommit_block = precommit_data.block
 
         if self._last_block.hash != precommit_block.prev_hash or \
                 self._last_block.height + 1 != precommit_block.height:
