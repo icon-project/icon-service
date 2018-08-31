b3d3f1cb82dbb5afb970c23395c1766221590b96
Chiwon Cho <goldworm@icon.foundation>
2018-08-31 22:28:22 +0900

    While process a tx, if the caught exception is not a kind of IconServiceBaseException, set failure.message to 'Unexpected error'

--- iconservice/icon_service_engine.py
+++ iconservice/icon_service_engine.py
@@ -810,7 +810,7 @@ class IconServiceEngine(ContextContainer):
             Logger.error(e, ICON_SERVICE_LOG_TAG)
 
             code = ExceptionCode.SERVER_ERROR
-            message = str(e)
+            message = 'Unexpected error'
 
         return TransactionResult.Failure(code, message)
 
