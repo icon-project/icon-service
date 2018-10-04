a69bcad7ed5c32749953a5e48cf251ad437fd2df
Chiwon Cho <goldworm@icon.foundation>
2018-10-05 01:06:24 +0900

    Update check_audit_ignore() with governance get_revision Apply legacy IconScoreDeployer to IconScoreDeployEngine with governance revision Fix an exception in IconScoreEngine._charge_transaction_fee() Update icx.send() to support db compatibility with governance revision

--- iconservice/deploy/icon_score_deployer.py
+++ iconservice/deploy/icon_score_deployer.py
@@ -105,3 +105,82 @@ class IconScoreDeployer(object):
             os.remove(archive_path)
         elif os.path.isdir(archive_path):
             shutil.rmtree(archive_path)
+
+    def deploy_legacy(self,
+                      address: 'Address',
+                      data: bytes,
+                      tx_hash: bytes) -> None:
+        """Install score.
+        Use 'address', 'block_height', and 'transaction_index' to specify the path where 'Score' will be installed.
+        :param address: score address
+        :param data: The byte value of the zip file.
+        :param tx_hash:
+        :return:
+        """
+
+        score_root_path = os.path.join(self.score_root_path, address.to_bytes().hex())
+        converted_tx_hash = f'0x{bytes.hex(tx_hash)}'
+        install_path = os.path.join(score_root_path, converted_tx_hash)
+
+        try:
+            if os.path.isfile(install_path):
+                raise ScoreInstallException(f'{install_path} is a file. Check your path.')
+
+            if os.path.isdir(install_path):
+                raise ScoreInstallException(f'{install_path} is a directory. Check {install_path}')
+
+            if not os.path.exists(install_path):
+                os.makedirs(install_path)
+
+            file_info_generator = IconScoreDeployer._extract_files_gen_legacy(data)
+            for name, file_info, parent_directory in file_info_generator:
+                if not os.path.exists(os.path.join(install_path, parent_directory)):
+                    os.makedirs(os.path.join(install_path, parent_directory))
+                with file_info as file_info_context, open(os.path.join(install_path, name), 'wb') as dest:
+                    contents = file_info_context.read()
+                    dest.write(contents)
+        except BaseException as e:
+            shutil.rmtree(install_path, ignore_errors=True)
+            raise e
+
+    @staticmethod
+    def _extract_files_gen_legacy(data: bytes):
+        """Yield (filename, file_information, parent_directory_name) tuple.
+
+        :param data: The byte value of the zip file.
+        :return:
+        """
+        try:
+            with zipfile.ZipFile(io.BytesIO(data)) as memory_zip:
+                memory_zip_infolist = memory_zip.infolist()
+                memory_zip_files_path_gen = (path.filename for path in memory_zip_infolist)
+                common_path_len = len(os.path.commonpath(memory_zip_files_path_gen))
+                start_index = common_path_len
+                if common_path_len == 0:
+                    start_index = -1
+                for zip_info in memory_zip.infolist():
+                    with memory_zip.open(zip_info) as file:
+                        file_path = zip_info.filename[start_index + 1:]
+                        file_name_start_index = file_path.rfind('/')
+                        parent_directory = file_path[:file_name_start_index]
+                        if file_path.find('__MACOSX') != -1:
+                            continue
+                        if file_path.find('__pycache__') != -1:
+                            continue
+                        if file_name_start_index == len(file_path) - 1:
+                            # continue when 'file_path' is a directory.
+                            continue
+                        if file_path.startswith('.') or file_path.find('/.') != -1:
+                            # continue when 'file_path' is hidden directory or hidden file.
+                            continue
+
+                        if file_name_start_index == -1:
+                            yield file_path, file, ''
+                        else:
+                            yield file_path, file, parent_directory
+        except zipfile.BadZipFile:
+            raise ScoreInstallExtractException("Bad zip file.")
+        except zipfile.LargeZipFile:
+            raise ScoreInstallExtractException("Large zip file.")
+        except Exception as e:
+            raise ScoreInstallExtractException(f'extract_files_gen error -> exception: {e}')
--- iconservice/iconscore/icx.py
+++ iconservice/iconscore/icx.py
@@ -15,9 +15,11 @@
 # limitations under the License.
 
 from typing import TYPE_CHECKING
+
+from ..base.address import Address, GOVERNANCE_SCORE_ADDRESS
+
 if TYPE_CHECKING:
     from .icon_score_context import IconScoreContext
-    from ..base.address import Address
 
 
 class Icx(object):
@@ -48,9 +50,24 @@ class Icx(object):
         """
         try:
             self.transfer(addr_to, amount)
+            if not addr_to.is_contract and self._is_icx_send_defective():
+                return False
             return True
         except:
             return False
 
     def get_balance(self, address: 'Address') -> int:
         return self._context.internal_call.get_icx_balance(address)
+
+    # noinspection PyBroadException
+    def _is_icx_send_defective(self) -> bool:
+        try:
+            governance_score = self._context.get_icon_score(GOVERNANCE_SCORE_ADDRESS)
+            if governance_score is not None:
+                if hasattr(governance_score, 'getVersion'):
+                    version = governance_score.getVersion()
+                    return version == '0.0.2'
+        except BaseException:
+            pass
+
+        return False
