9be51bd988fae55f76a4cf53542c9642ce52fc96
Boyeon.Choi <boyeon.choi@icon.foundation>
2018-10-05 11:53:23 +0900

    Add more external functions
    - To make unit tests support to not only the read-only external function but the not read-only ones.

--- tests/integrate_test/test_samples/test_score_api/test_score_base/test_score_base.py
+++ tests/integrate_test/test_samples/test_score_api/test_score_base/test_score_base.py
@@ -54,13 +54,27 @@ class TestScoreBase(IconScoreBase):
         self.Changed(value)
 
     @external(readonly=True)
-    def test_sha3_256(self, data: bytes) -> bytes:
+    def test_sha3_256_readonly(self, data: bytes) -> bytes:
         return sha3_256(data)
 
+    @external
+    def test_sha3_256(self, data: bytes):
+        sha3_256(data)
+        self._value.set(10**18)
+        self.Changed(10**18)
+
     @external(readonly=True)
-    def test_json_dumps(self) -> str:
+    def test_json_dumps_readonly(self) -> str:
         data = {"key1": 1, "key2": 2, "key3": "value3"}
         return json_dumps(data)
+
+    @external
+    def test_json_dumps(self):
+        data = {"key1": 1, "key2": 2, "key3": "value3"}
+        return_value = json_dumps(data)
+        assert type(return_value) == str
+        self._value.set(10**18)
+        self.Changed(10**18)
 
     @external(readonly=True)
     def test_json_dumps_none(self) -> str:
@@ -68,32 +82,79 @@ class TestScoreBase(IconScoreBase):
         return json_dumps(data)
 
     @external(readonly=True)
-    def test_json_loads(self) -> str:
+    def test_json_loads_readonly(self) -> str:
         data = json_dumps({"key1": 1, "key2": 2, "key3": "value3"})
         return json_loads(data)
 
+    @external
+    def test_json_loads(self):
+        data = json_dumps({"key1": 1, "key2": 2, "key3": "value3"})
+        return_value = json_loads(data)
+        assert type(return_value) == dict
+        self._value.set(10**18)
+        self.Changed(10**18)
+
     @external(readonly=True)
-    def test_is_score_active(self, address: Address) -> bool:
+    def test_is_score_active_readonly(self, address: Address) -> bool:
         return self.is_score_active(address)
 
+    @external
+    def test_is_score_active(self, address: Address):
+        assert self.is_score_active(address) in (True, False)
+        self._value.set(10**18)
+        self.Changed(10**18)
+
     @external(readonly=True)
-    def test_get_owner(self, address: Address) -> bool:
+    def test_get_owner_readonly(self, address: Address) -> Address:
         return self.get_owner(address)
 
-    @external(readonly=True)
-    def test_create_interface_score(self, address: Address) -> str:
-        return self.create_interface_score(address, TestScoreBaseInterface)
+    @external
+    def test_get_owner(self, address: Address) -> bool:
+        assert type(self.get_owner(address)) == Address
+        self._value.set(10**18)
+        self.Changed(10**18)
 
-    @external(readonly=False)
+    @external(readonly=True)
+    def test_create_interface_score_readonly(self, address: Address) -> bool:
+        return_value = self.create_interface_score(address, TestScoreBaseInterface)
+        assert type(type(return_value)) == type(TestScoreBaseInterface)
+        return True
+
+    @external
+    def test_create_interface_score(self, address: Address):
+        self.create_interface_score(address, TestScoreBaseInterface)
+        self._value.set(10 ** 18)
+        self.Changed(10 ** 18)
+
+    @external(readonly=True)
+    def test_deploy_readonly(self, tx_hash: bytes) -> bool:
+        self.deploy(tx_hash)
+        return True
+
+    @external
     def test_deploy(self, tx_hash: bytes):
         self.deploy(tx_hash)
 
     @external(readonly=True)
-    def test_get_tx_hashes_by_score_address(self, address: Address) -> bytes:
+    def test_get_tx_hashes_by_score_address_readonly(self, address: Address) -> bytes:
         return self.get_tx_hashes_by_score_address(address)
 
+    @external
+    def test_get_tx_hashes_by_score_address(self, address: Address):
+        tx_hashes = self.get_tx_hashes_by_score_address(address)
+        assert type(tx_hashes) == tuple
+        self._value.set(10 ** 18)
+        self.Changed(10 ** 18)
+
     @external(readonly=True)
-    def test_get_score_address_by_tx_hash(self, tx_hash: bytes) -> Address:
+    def test_get_score_address_by_tx_hash_readonly(self, tx_hash: bytes) -> Address:
         return self.get_score_address_by_tx_hash(tx_hash)
 
+    @external
+    def test_get_score_address_by_tx_hash(self, tx_hash: bytes):
+        score_address = self.get_score_address_by_tx_hash(tx_hash)
+        if score_address:
+            assert type(score_address) == Address
+        self._value.set(10 ** 18)
+        self.Changed(10 ** 18)
 
