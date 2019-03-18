f168992c6e388bfabbb120017d3213d937876ffe
Yongwoo Lee <whylee259@gmail.com>
2019-03-18 15:44:13 +0900

    Add `Deposit` module
    * Accept revision value patch in mock_generator
    * Write skeleton tests for fee sharing
    * Adds CRUD APIs for Fee and Deposit
    * Adds unit tests for CRUD of Deposit and Fee
    * Uses the new module 'msgpack' to make a Python object serializable

---
+++ iconservice/fee/deposit.py
@@ -0,0 +1,119 @@
+from typing import Optional
+
+from ..base.address import Address
+from ..utils.msgpack_for_db import MsgPackForDB
+
+
+class Deposit(object):
+    """
+    Deposit Information Class
+    implementing functions to serialize, deserialize and convert to dict type.
+    """
+    def __init__(self, deposit_id: bytes = None, score_address: 'Address' = None, sender: 'Address' = None,
+                 deposit_amount: int = 0, deposit_used: int = 0, created: int = 0, expires: int = 0,
+                 virtual_step_issued: int = 0, virtual_step_used: int = 0, prev_id: bytes = None,
+                 next_id: bytes = None):
+        # deposit id, should be tx hash of deposit transaction
+        self.id = deposit_id
+        # target SCORE address
+        self.score_address = score_address
+        # sender address
+        self.sender = sender
+        # deposit amount of ICXs in loop
+        self.deposit_amount = deposit_amount
+        # used amount of deposited ICXs in loop
+        self.deposit_used = deposit_used
+        # created time in block
+        self.created = created
+        # expires time in block
+        self.expires = expires
+        # issued amount of virtual STEPs
+        self.virtual_step_issued = virtual_step_issued
+        # used amount of virtual STEPs
+        self.virtual_step_used = virtual_step_used
+        # previous id of this deposit
+        self.prev_id = prev_id
+        # next id of this deposit
+        self.next_id = next_id
+
+    @staticmethod
+    def from_bytes(buf: bytes):
+        """Creates Deposit object from bytes data.
+
+        :param buf: deposit info in bytes
+        :return: deposit object
+        """
+        data: list = MsgPackForDB.loads(buf)
+
+        deposit = Deposit()
+        deposit.score_address = data[0]
+        deposit.sender = data[1]
+        deposit.deposit_amount = data[2]
+        deposit.deposit_used = data[3]
+        deposit.created = data[4]
+        deposit.expires = data[5]
+        deposit.virtual_step_issued = data[6]
+        deposit.virtual_step_used = data[7]
+        deposit.prev_id = data[8]
+        deposit.next_id = data[9]
+
+        return deposit
+
+    def to_bytes(self) -> bytes:
+        """Converts Deposit object into bytes.
+
+        :return: deposit info in bytes
+        """
+        data: list = [self.score_address,
+                      self.sender,
+                      self.deposit_amount,
+                      self.deposit_used,
+                      self.created,
+                      self.expires,
+                      self.virtual_step_issued,
+                      self.virtual_step_used,
+                      self.prev_id,
+                      self.next_id]
+
+        return MsgPackForDB.dumps(data)
+
+    def to_dict(self, casing: Optional = None) -> dict:
+        """Returns properties as dict.
+
+        :param casing: a kind of functions to convert one casing notation to another
+        :return: deposit info in dict
+        """
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            # Excludes properties which have `None` value
+            if value is None:
+                continue
+
+            new_key = casing(key) if casing else key
+            new_dict[new_key] = value
+
+        return new_dict
+
+    def __eq__(self, other) -> bool:
+        """operator == overriding
+
+        :param other: (Deposit)
+        """
+        return isinstance(other, Deposit) \
+               and self.score_address == other.score_address \
+               and self.sender == other.sender \
+               and self.deposit_amount == other.deposit_amount \
+               and self.deposit_used == other.deposit_used \
+               and self.created == other.created \
+               and self.expires == other.expires \
+               and self.virtual_step_issued == other.virtual_step_issued \
+               and self.virtual_step_used == other.virtual_step_used \
+               and self.prev_id == other.prev_id \
+               and self.next_id == other.next_id
+
+    def __ne__(self, other) -> bool:
+        """operator != overriding
+
+        :param other: (Deposit)
+        """
+        return not self.__eq__(other)
---
+++ tests/test_fee_sharing.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 ICON Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import unittest
+
+from iconservice.base.address import AddressPrefix, Address
+from iconservice.icon_constant import LATEST_REVISION
+from iconservice.iconscore.icon_score_context import ContextContainer
+from tests.mock_generator import generate_inner_task, clear_inner_task, create_request, ReqData, create_query_request, \
+    QueryData
+
+TEST_ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+
+
+class TestFeeSharing(unittest.TestCase):
+
+    def setUp(self):
+        self._inner_task = generate_inner_task(LATEST_REVISION)
+        self.from_ = Address.from_data(AddressPrefix.EOA, os.urandom(20))
+        self.to = Address.from_string('cx' + '0' * 40)
+        self.score = Address.from_string('cx' + '1' * 40)
+
+    def tearDown(self):
+        ContextContainer._clear_context()
+        clear_inner_task()
+
+    def _inner_task_invoke(self, request) -> dict:
+        # Clear cached precommit data before calling inner_task._invoke
+        self._inner_task._icon_service_engine._precommit_data_manager.clear()
+        return self._inner_task._invoke(request)
+
+    def _inner_task_query(self, request) -> dict:
+        return self._inner_task._query(request)
+
+    def test_set_fee_share(self):
+        ratio = 50
+        tx_hash = bytes.hex(os.urandom(32))
+
+        data = {
+            'method': 'setFeeShare',
+            'params': {
+                '_score': str(self.score),
+                '_ratio': ratio
+            }
+        }
+
+        expected_event_log = {
+            "scoreAddress": str(self.to),
+            "indexed": [
+                "SetFeeShare(Address,int)",
+                str(self.score),
+            ],
+            "data": [
+                hex(ratio)
+            ]
+        }
+
+        request = create_request([
+            ReqData(tx_hash, self.from_, self.to, 'call', data),
+        ])
+
+        result = self._inner_task_invoke(request)
+        tx_result = result['txResults'][tx_hash]
+
+        self.assertEqual(expected_event_log, tx_result['eventLogs'])
+        self.assertEqual('0x1', tx_result['status'])
+
+        return ratio
+
+    def test_get_fee_share(self):
+        ratio = self.test_set_fee_share()
+
+        data = {
+            "method": "getFeeShare",
+            "params": {
+                "_score": str(self.score)
+            }
+        }
+
+        expected_response = {
+            "ratio": ratio
+        }
+
+        request = create_query_request(QueryData(self.from_, self.to, "call", data))
+        result = self._inner_task_query(request)
+
+        self.assertEqual(expected_response, result)
+
+    def test_create_deposit(self):
+        tx_hash = bytes.hex(os.urandom(32))
+        term, amount = 50, 5000
+        data = {
+            'method': 'createDeposit',
+            'params': {
+                '_score': str(self.score),
+                '_term': term,
+                '_amount': amount
+            }
+        }
+
+        expected_event_log = {
+            "scoreAddress": str(self.to),
+            "indexed": [
+                "CreateDeposit(bytes,Address,Address,amount,term)",
+                tx_hash,
+                str(self.score),
+                str(self.from_)
+            ],
+            "data": [
+                hex(amount),
+                hex(term)
+            ]
+        }
+
+        request = create_request([
+            ReqData(tx_hash, self.from_, self.to, 'call', data),
+        ])
+
+        result = self._inner_task_invoke(request)
+        tx_result = result['txResults'][tx_hash]
+
+        self.assertEqual('0x1', tx_result['status'])
+        self.assertEqual(expected_event_log, tx_result['eventLogs'])
+
+        return tx_hash
+
+    def test_destroy_deposit(self):
+        deposit_id = self.test_create_deposit()
+        amount, penalty = 4700, 300
+
+        tx_hash = bytes.hex(os.urandom(32))
+
+        data = {
+            'method': 'destroyDeposit',
+            'params': {
+                '_id': deposit_id
+            }
+        }
+
+        expected_event_log = {
+            "scoreAddress": str(self.to),
+            "indexed": [
+                "DepositDestroyed(bytes,Address,Address,int,int)",
+                deposit_id,
+                str(self.score),
+                str(self.from_)
+            ],
+            "data": [
+                hex(amount),
+                hex(penalty)
+            ]
+        }
+
+        request = create_request([
+            ReqData(tx_hash, self.from_, self.to, 'call', data),
+        ])
+
+        result = self._inner_task_invoke(request)
+        tx_result = result['txResults'][tx_hash]
+
+        self.assertEqual('0x1', tx_result['status'])
+        self.assertEqual(expected_event_log, tx_result['eventLogs'])
+
+    def test_get_deposit(self):
+        deposit_id = self.test_create_deposit()
+        current_block_height = 100
+
+        data = {
+            'method': 'getDeposit',
+            'params': {
+                '_id': deposit_id
+            }
+        }
+
+        expected_result = {
+            "_id": deposit_id,
+            "_score": str(self.score),
+            "_from": str(self.from_),
+            "amount": hex(5000),
+            "_createdAt": f"{hex(current_block_height)}",
+            "_expiresIn": f"{hex(current_block_height+50)}",
+            "_virtualStepIssued": "f(d,a)",
+            "_virtualStepUsed": "used vs"
+        }
+        request = create_query_request(QueryData(self.from_, self.to, "call", data))
+
+        result = self._inner_task_query(request)
+
+        self.assertEqual(expected_result, result)
+
+    def test_get_score_info(self):
+        self.test_set_fee_share()
+        deposit_id = self.test_create_deposit()
+        current_block_height = 100
+
+        data = {
+            'method': "getScoreInfo",
+            "params": {
+                "_score": str(self.score)
+            }
+        }
+
+        expected_result = {
+            "_address": str(self.score),
+            "_totalVirtualStepIssued": hex(5000),
+            "_totalVirtualStepUsed": hex(0),
+            "_deposits": [
+                {
+                    "_id": deposit_id,
+                    "_score": str(self.score),
+                    "_from": str(self.from_),
+                    "amount": hex(5000),
+                    "_createdAt": f"{current_block_height}",
+                    "_expiresIn": f"{current_block_height + 50}",
+                    "_virtualStepIssued": "f(d,a)",
+                    "_virtualStepUsed": "used vs"
+                 }
+            ]
+        }
+
+        request = create_query_request(QueryData(self.from_, self.to, "call", data))
+        result = self._inner_task_query(request)
+        self.assertEqual(expected_result, result)
+
+    def test_available_steps(self):
+        self.test_set_fee_share()
+        self.test_create_deposit()
+
+        data = {
+            'method': "availableStep",
+            "params": {
+                "_score": str(self.score)
+            }
+        }
+
+        expected_result = {
+            "availableStep": hex(5000)
+        }
+
+        request = create_query_request(QueryData(self.from_, self.to, "call", data))
+        result = self._inner_task_query(request)
+        self.assertEqual(expected_result, result)
+
+    def test_transaction_result_on_sharing_fee(self):
+        self.test_set_fee_share()
+        self.test_create_deposit()
+        tx_hash = bytes.hex(os.urandom(32))
+
+        data = {
+            "method": "transfer",
+            "params": {
+                "_to": f"hx{'2'*40}",
+                "_amount": hex(100)
+            }
+        }
+
+        request = create_request([ReqData(tx_hash, self.from_, str(self.score), "call", data)])
+        result = self._inner_task_invoke(request)
+
+        expected_event_log = {
+            "scoreAddress": str(self.score),
+            "indexed": [
+                "Transfer(Address,Address,int)",
+                str(self.from_),
+                f"hx{'2'*40}"
+            ],
+            "data": [
+                hex(100)
+            ]
+        }
+
+        expected_detail_step_used = [
+            {
+                "address": str(self.from_),
+                "stepUsed": hex(9000)
+            },
+            {
+                "address": str(self.score),
+                "stepUsed": hex(9000)
+            }
+        ]
+
+        tx_result = result['txResults'][tx_hash]
+
+        self.assertEqual(expected_event_log, tx_result['eventLogs'])
+        self.assertEqual('0x1', tx_result['status'])
+        self.assertEqual(expected_detail_step_used, tx_result['detailStepUsed'])
+
