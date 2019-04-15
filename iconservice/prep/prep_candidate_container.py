47e192c7f72441f9b58fb8617f22356e17e77075
leeheonseung <mycom22@icon.foundation>
2019-04-15 22:19:37 +0900

    add prep candidate linked list

---
+++ iconservice/prep/prep_candidate_container.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 ICON Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from functools import cmp_to_key
+from threading import Lock
+from typing import TYPE_CHECKING, List
+
+from .prep_candidate_info_for_sort import PRepCandidateInfoForSort
+from .prep_candidate_linked_list import PRepCandidateLinkedList
+from ..base.exception import InvalidParamsException
+
+if TYPE_CHECKING:
+    from ..base.address import Address
+
+
+class PRepCandiateInfoMapper(object):
+    def __init__(self):
+        self._prep_candidate_objects: dict = {}
+        self._lock = Lock()
+
+    def __setitem__(self, key: 'Address', value: 'PRepCandidateInfoForSort'):
+        with self._lock:
+            self._prep_candidate_objects[key] = value
+
+    def __getitem__(self, key: 'Address') -> 'PRepCandidateInfoForSort':
+        with self._lock:
+            return self._prep_candidate_objects[key]
+
+    def __delitem__(self, key: 'Address'):
+        with self._lock:
+            del self._prep_candidate_objects[key]
+
+    def __contains__(self, address: 'Address') -> bool:
+        with self._lock:
+            return address in self._prep_candidate_objects
+
+    def to_genesis_sorted_list(self) -> List['PRepCandidateInfoForSort']:
+        with self._lock:
+            return sorted(self._prep_candidate_objects.values(), key=cmp_to_key(PRepCandidateInfoForSort.compare_key))
+
+
+class PRepCandidateSortedInfos(object):
+
+    def __init__(self):
+        self._prep_candidate_objects: 'PRepCandidateLinkedList' = PRepCandidateLinkedList()
+        self._lock = Lock()
+        self._init = False
+
+    def genesis_update(self, prep_objs: List['PRepCandidateInfoForSort']):
+        with self._lock:
+            if self._init:
+                raise InvalidParamsException(f'Invalid instance update : init is already True')
+
+            for obj in prep_objs:
+                self._prep_candidate_objects.append(obj)
+            self._init = True
+
+    def get(self) -> list:
+        with self._lock:
+            tmp: list = []
+            for n in self._prep_candidate_objects:
+                tmp.append(n.data)
+            return tmp
+
+    def add_info(self, new_info: 'PRepCandidateInfoForSort'):
+        with self._lock:
+            self._prep_candidate_objects.append(new_info)
+
+    def del_info(self, address: 'Address'):
+        with self._lock:
+            self._prep_candidate_objects.remove(address)
+
+    def update_info(self, address: 'Address', update_total_delegated: int):
+        with self._lock:
+            self._prep_candidate_objects.update(address, update_total_delegated)
--- tests/prep/test_prep_candidate.py
+++ tests/prep/test_prep_candidate.py
@@ -15,12 +15,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import unittest
 
 from iconservice import Address
 from iconservice.prep.prep_candidate import PRepCandidate
+from iconservice.prep.prep_candidate_container import PRepCandidateSortedInfos, PRepCandiateInfoMapper
+from iconservice.prep.prep_candidate_info_for_sort import PRepCandidateInfoForSort
 from tests import create_address
+
+from random import randint
 
 
 class TestPrepCandidate(unittest.TestCase):
@@ -32,17 +35,98 @@ class TestPrepCandidate(unittest.TestCase):
         actual_prep: 'PRepCandidate' = prep.from_bytes(data, address)
         self.assertEqual(prep, actual_prep)
 
-        prep.network_info = "network_info"
         prep.name = "name"
-        prep.url = "url"
+        prep.email = "email"
+        prep.website = "website"
+        prep.json = "json"
+        prep.ip = "192.168.0.1"
         prep.block_height = 10
-        prep.timestamp = 2000
-        prep.governance.incentiveRep = 200
+        prep.tx_index = 1000
+        prep.gv.incentiveRep = 200
 
-        prep: 'PRepCandidate' = PRepCandidate(address)
         data: bytes = prep.to_bytes()
         actual_prep: 'PRepCandidate' = prep.from_bytes(data, address)
         self.assertEqual(prep, actual_prep)
+
+    def test_prep_candidate_info_for_sort(self):
+        mapper = PRepCandiateInfoMapper()
+        for i in range(5):
+            addr: 'Address' = create_address()
+            name: str = f'name{i}'
+            total_delegated: int = randint(1, 10)
+            block_height: int = randint(1, 10)
+            tx_index: int = i
+            info: 'PRepCandidateInfoForSort' = PRepCandidateInfoForSort(addr,
+                                                                        name,
+                                                                        block_height,
+                                                                        tx_index)
+            info.update(total_delegated)
+            mapper[addr] = info
+
+        sorted_list: list = mapper.to_genesis_sorted_list()
+
+        infos = PRepCandidateSortedInfos()
+        infos.genesis_update(sorted_list)
+        self._custom_prt(infos.get())
+
+        info: 'PRepCandidateInfoForSort' = PRepCandidateInfoForSort(create_address(),
+                                                                    "new_name1",
+                                                                    30,
+                                                                    0)
+        infos.add_info(info)
+        infos.update_info(info.address, 50)
+        self._custom_prt(infos.get())
+
+        infos.update_info(info.address, 40)
+        self._custom_prt(infos.get())
+
+        infos.update_info(info.address, 5)
+        self._custom_prt(infos.get())
+
+        infos.update_info(info.address, 10)
+        self._custom_prt(infos.get())
+
+        infos.del_info(info.address)
+        self._custom_prt(infos.get())
+
+    def test_prep_candidate_info_for_sort2(self):
+        mapper = PRepCandiateInfoMapper()
+
+        total_delegatedes = [10, 8, 7, 7, 2]
+        block_heights = [3, 10, 3, 4, 9]
+        tx_indexes = [2, 0, 1, 4, 3]
+
+        for i in range(5):
+            addr: 'Address' = create_address()
+            name: str = f'name{i}'
+            total_delegated: int = total_delegatedes[i]
+            block_height: int = block_heights[i]
+            tx_index: int = tx_indexes[i]
+            info: 'PRepCandidateInfoForSort' = PRepCandidateInfoForSort(addr,
+                                                                        name,
+                                                                        block_height,
+                                                                        tx_index)
+            info.update(total_delegated)
+            mapper[addr] = info
+
+        sorted_list: list = mapper.to_genesis_sorted_list()
+
+        infos = PRepCandidateSortedInfos()
+        infos.genesis_update(sorted_list)
+        self._custom_prt(infos.get())
+
+        info: 'PRepCandidateInfoForSort' = PRepCandidateInfoForSort(create_address(),
+                                                                    "new_name1",
+                                                                    30,
+                                                                    0)
+        infos.add_info(info)
+        infos.update_info(info.address, 10)
+        self._custom_prt(infos.get())
+
+    def _custom_prt(self, infos: list):
+        print("======")
+        for info in infos:
+            print(info.name, info.to_order_list())
 
 
 if __name__ == '__main__':
