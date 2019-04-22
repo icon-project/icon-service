2bb45bae0d2e61051fdffead070c1528b733f33c
soobokjin <42569411+soobokjin@users.noreply.github.com>
2019-04-22 17:55:09 +0900

    IS-455: Move icx issue amount calculating logic from icon service engine to iiss engine (#127)
    * Implement 'issue_amount' method on IconScoreInnerTask class
    Loopchain will call this method before request 'invoke' (to make coinbase transaction)
    
    * Change the name of 'issue_amount' method
    before: issue_amount
    after: get_issue_info
    
    * Modify 'get_issue_info' channel logic
    To get issue information from iiss engine, context is required as iiss engine have to access db. So change the logic
    
    * Implement coinbase transaction validation logic
    Change the name of module
    iiss_issue_formula -> icx_issue_formula
    
    * Add __init__.py in iiss package
    
    * Implement ICX issue transaction validation logic
    1. Implement type converter template of ICC issue transaction
    2. Implement validation logic in _invoke_issue_request method
    
    * need to test this codes
    
    * Implement unit tests and integrate test about icx issue transaction validation logic.
    1. Implement unit tests about issue method in icx engine
    2. Implement integrate tests about issue transaction validation logic
    
    * Implement unit tests about type converter
    
    * 1. Implement logic about getting previous block contributors.
    2. Fix unit, integrate tests
    reason: brake because of issue logic and validator
    
    * Apply revision to icx issue logic
    before revision 5:
    get_issue_info: return 0 as a value (temp, format and data will be changed after demo)
    invoke: doesn't check if the 0 index tx is issue tx or not
    after revision 5:
    get_issue_info: return valid issue related data
    invoke: check if the 0 index tx is issue tx or not
    
    * Change the parameters position of PrecommitData class
    
    * minor fix
    
    * Add unit tests about type converter templete
    1. Add tests about prevBlockContributors
    
    * Minor change
    get_issue_info thread flag -> QUERY
    
    * Minor change in icx_engine
    
    * Move calculating icx issue amount logic to iiss_engine temporary for demo
    
    * Add prep info to config
    
    * minor fix
    
    * Refactor issue transaction validate & icx issue logic

--- iconservice/icon_inner_service.py
+++ iconservice/icon_inner_service.py
@@ -24,7 +24,7 @@ from iconservice.base.block import Block
 from iconservice.base.exception import ExceptionCode, IconServiceBaseException
 from iconservice.base.type_converter import TypeConverter, ParamType
 from iconservice.icon_constant import ICON_INNER_LOG_TAG, ICON_SERVICE_LOG_TAG, \
-    EnableThreadFlag, ENABLE_THREAD_FLAG
+    EnableThreadFlag, ENABLE_THREAD_FLAG, ConfigKey
 from iconservice.icon_service_engine import IconServiceEngine
 from iconservice.utils import check_error_response, to_camel_case
 
@@ -62,7 +62,34 @@ class IconScoreInnerTask(object):
 
     @message_queue_task
     async def hello(self):
+        response = MakeResponse.make_response({"isIssuable": True,
+                                               "pRepList": self._conf[ConfigKey.IISS_PREP_LIST]
+                                               })
         Logger.info('icon_score_hello', ICON_INNER_LOG_TAG)
+        return response
+
+    @message_queue_task
+    async def get_issue_info(self):
+        if self._is_thread_flag_on(EnableThreadFlag.QUERY):
+            loop = get_event_loop()
+            return await loop.run_in_executor(self._thread_pool[THREAD_QUERY],
+                                              self._get_issue_info)
+        else:
+            return self._get_issue_info()
+
+    def _get_issue_info(self):
+        query_method_name = "iiss_get_issue_info"
+        response = None
+        try:
+            value = self._icon_service_engine.query(query_method_name, {})
+            response = MakeResponse.make_response(value)
+        # todo: add except after implement formular method
+        except Exception as e:
+            self._log_exception(e, ICON_SERVICE_LOG_TAG)
+            response = MakeResponse.make_error_response(ExceptionCode.SYSTEM_ERROR, str(e))
+        finally:
+            Logger.info(f'icx issue response with {response}', ICON_INNER_LOG_TAG)
+            return response
 
     @message_queue_task
     async def get_issue_info(self):
@@ -281,7 +308,7 @@ class MakeResponse:
             return TypeConverter.convert_type_reverse(response)
 
     @staticmethod
-    def make_error_response(code: Any, message: str):
+    def make_error_response(code: Any, message: str) -> dict:
         _code: int = int(code) + 32000
         return {'error': {'code': _code, 'message': message}}
 
