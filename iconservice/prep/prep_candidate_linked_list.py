ce151801d292d4a3c1079eda220c68b9696be965
leeheonseung <mycom22@icon.foundation>
2019-04-29 11:21:27 +0900

    arrange prep candidate container

--- iconservice/prep/prep_candidate_linked_list.py
+++ iconservice/prep/prep_candidate_linked_list.py
@@ -41,19 +41,79 @@ class PRepCandidateLinkedList:
         self._size: int = 0
 
     def append(self, data: 'PRepCandidateInfoForSort'):
-        new_node = Node(data)
-        self._append(new_node)
+        node = Node(data)
+        if self._is_empty():
+            self._add_init_node(node)
+        else:
+            self._append(node)
 
     def remove(self, address: 'Address'):
-        node: 'Node' = self._find_by_address(address)
+        if self._is_empty():
+            return
+
+        node: 'Node' = self._get(address)
         if node is None:
             return
-        self._remove_node(node)
 
-    def _find_by_address(self, address: 'Address') -> Optional['Node']:
-        if self._head is None:
-            return None
+        self._remove(node)
 
+    def update(self, address: 'Address', new_total_delegated: int):
+        if self._is_empty():
+            return
+
+        update_node: 'Node' = self._get(address)
+        if update_node is None:
+            raise InvalidParamsException(f"Fail update_sort: node is None")
+
+        self._remove(update_node)
+        update_node.data.update(new_total_delegated)
+
+        if self._size == 0:
+            self._add_init_node(update_node)
+        else:
+            src_node: 'Node' = None
+            it: 'Node' = self._tail
+            while it:
+                if update_node.data < it.data:
+                    src_node = it
+                    break
+                else:
+                    it: 'Node' = it.prev
+
+            if src_node is None:
+                # head node
+                self._insert_head_node(update_node)
+            elif src_node == self._tail:
+                # tail node
+                self._append(update_node)
+            else:
+                self._insert_next_to_src_node(src_node, update_node)
+
+    def clear(self):
+        self._head = None
+        self._tail = None
+        self._size = 0
+
+    def _is_empty(self):
+        return self._size == 0
+
+    def _add_init_node(self, node: 'Node'):
+        assert self._head is None
+        assert self._tail is None
+        assert self._is_empty()
+
+        self._head = node
+        self._tail = node
+        self._size += 1
+
+    def _insert_head_node(self, node: 'Node'):
+        prev_head = self._head
+        prev_head.prev = node
+        self._head = node
+        self._head.next = prev_head
+        self._size += 1
+
+    def _get(self, address: 'Address') -> Optional['Node']:
         node: 'Node' = self._head
         while node:
             if node.data.address == address:
@@ -62,79 +122,45 @@ class PRepCandidateLinkedList:
                 node: 'Node' = node.next
         return None
 
-    def update(self, address: 'Address', new_total_delegated: int):
-        update_node: 'Node' = self._find_by_address(address)
-        if update_node is None:
-            raise InvalidParamsException(f"Fail update_sort: node is None")
+    def _append(self, node: 'Node'):
+        assert self._tail is not None
 
-        update_node.data.update(new_total_delegated)
-        self._remove_node(update_node)
-
-        node: 'Node' = None
-        it: 'Node' = self._tail
-        while it:
-            if update_node.data < it.data:
-                node = it
-                break
-            else:
-                it: 'Node' = it.prev
-
-        self._insert_next_src_node(node, update_node)
-
-    def clear(self):
-        self._head = None
-        self._tail = None
-        self._size = 0
-
-    def _append(self, new_node: 'Node'):
-        if self._tail is None:
-            self._head = new_node
-            self._tail = new_node
-        else:
-            prev_tail = self._tail
-            self._tail = new_node
-            prev_tail.next = new_node
-            new_node.prev = prev_tail
+        prev_tail = self._tail
+        self._tail = node
+        prev_tail.next = node
+        node.prev = prev_tail
         self._size += 1
 
-    def _insert_next_src_node(self, src_node: 'Node', new_node: 'Node'):
-        if src_node is None:
-            # side
-            if self._head is None:
-                # first
-                self._head = new_node
-                self._tail = new_node
-            else: #
-                prev_head = self._head
-                prev_head.prev = new_node
-                self._head = new_node
-                new_node.next = prev_head
-        elif src_node.next is None:
-            self._tail = new_node
-            new_node.prev = src_node
-            src_node.next = new_node
-        else:
-            next_node = src_node.next
-            new_node.next = next_node
-            next_node.prev = new_node
+    def _insert_next_to_src_node(self, src_node: 'Node', node: 'Node'):
+        assert src_node is not None
+        assert src_node != self._tail
 
-            new_node.prev = src_node
-            src_node.next = new_node
+        next_node = src_node.next
+        next_node.prev = node
+
+        node.next = next_node
+        node.prev = src_node
+
+        src_node.next = node
         self._size += 1
 
-    def _remove_node(self, node: 'Node'):
+    def _remove(self, node: 'Node'):
+        assert not self._is_empty()
 
         prev_node: 'Node' = node.prev
         next_node: 'Node' = node.next
+
         node.prev = None
         node.next = None
 
         if prev_node is None:
+            # first node
             self._head = next_node
         else:
             prev_node.next = next_node
 
         if next_node is None:
+            # last node
             self._tail = prev_node
         else:
             next_node.prev = prev_node
