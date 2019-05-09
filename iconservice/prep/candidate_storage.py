f61b51ddd502c37c679c4b906575ce4894b2b5d7
BOBO <vonvoyage27@gmail.com>
2019-05-09 11:20:04 +0900

    Modify 'prep' package and module name (#137)
    * Remove a prefix with 'prep_' on related package and module
    * Optimize import packages

--- iconservice/prep/candidate_storage.py
+++ iconservice/prep/candidate_storage.py
@@ -14,8 +14,8 @@
 
 from typing import TYPE_CHECKING
 
+from .candidate import Candidate
 from ..base.exception import InvalidParamsException
-from .prep_candidate import PRepCandidate
 
 if TYPE_CHECKING:
     from ..base.address import Address
@@ -23,7 +23,7 @@ if TYPE_CHECKING:
     from ..iconscore.icon_score_context import IconScoreContext
 
 
-class PRepCandidateStorage(object):
+class CandidateStorage(object):
 
     def __init__(self, db: 'ContextDatabase'):
         """Constructor
@@ -42,25 +42,25 @@ class PRepCandidateStorage(object):
                      context: 'IconScoreContext',
                      address: 'Address') -> bool:
 
-        key: bytes = PRepCandidate.make_key(address)
+        key: bytes = Candidate.make_key(address)
         return self._db.get(context, key) is not None
 
     def get_candidate(self,
                       context: 'IconScoreContext',
-                      address: 'Address') -> 'PRepCandidate':
+                      address: 'Address') -> 'Candidate':
 
-        key: bytes = PRepCandidate.make_key(address)
+        key: bytes = Candidate.make_key(address)
         value: bytes = self._db.get(context, key)
         if value is None:
             raise InvalidParamsException("get_candidate: value is None")
-        candidate: 'PRepCandidate' = PRepCandidate.from_bytes(value, address)
+        candidate: 'Candidate' = Candidate.from_bytes(value, address)
         return candidate
 
     def put_candidate(self,
                       context: 'IconScoreContext',
-                      prep_candidate: 'PRepCandidate'):
+                      prep_candidate: 'Candidate'):
 
-        key: bytes = PRepCandidate.make_key(prep_candidate.address)
+        key: bytes = Candidate.make_key(prep_candidate.address)
         value: bytes = prep_candidate.to_bytes()
         self._db.put(context, key, value)
 
@@ -68,8 +68,8 @@ class PRepCandidateStorage(object):
                          context: 'IconScoreContext',
                          address: 'Address'):
 
-        key: bytes = PRepCandidate.make_key(address)
+        key: bytes = Candidate.make_key(address)
         self._db.delete(context, key)
 
-    def get_prep_candiates(self) -> iter:
-        return self._db.key_value_db.get_sub_db(PRepCandidate.prefix).iterator()
+    def get_prep_candidates(self) -> iter:
+        return self._db.key_value_db.get_sub_db(Candidate.prefix).iterator()
