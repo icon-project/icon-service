6f529a313857775af869052e7da80bd658ac5ea1
inwonkim <inwonkim@icon.foundation>
2019-06-19 16:07:14 +0900

    Change the return value of get_preps to follow the api specification

--- iconservice/inner_call/__init__.py
+++ iconservice/inner_call/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import hashlib
 from typing import TYPE_CHECKING
 
 from ..icon_constant import PREP_COUNT
@@ -22,36 +23,42 @@ if TYPE_CHECKING:
     from ..prep.data.prep_container import PRepContainer
 
 
+def get_preps_root_hash(prep_id_list: list) -> bytes:
+    return hashlib.sha3_256(b''.join(prep_id_list)).digest()
+
+
 def get_preps(context: IconScoreContext):
     preps: 'PRepContainer' = context.engine.prep.preps
     prep_result = []
+    prep_ids_in_bytes = []
 
     for prep in preps:
         p_rep: 'PRep' = context.engine.prep.preps.get(prep.address)
         data = {
             "id": str(p_rep.address),
             "publicKey": f"0x{bytes.hex(p_rep.public_key)}",
-            "p2pEndPoint": p_rep.p2p_end_point,
-            "irep": hex(p_rep.incentive_rep),
-            "irepUpdateBlockHeight": hex(p_rep.incentive_rep_block_height)
+            "p2pEndPoint": p_rep.p2p_end_point
         }
         prep_result.append(data)
+        prep_ids_in_bytes.append(p_rep.address.to_bytes())
 
         if len(prep_result) == PREP_COUNT:
             break
 
     block: 'Block' = context.storage.icx.last_block
+    root_hash = get_preps_root_hash(prep_ids_in_bytes)
 
     return {
         "result": {
             "blockHeight": 0 if block is None else block.height,
-            "preps": prep_result
+            "preps": prep_result,
+            "rootHash": f"0x{root_hash.hex()}"
         }
     }
 
 
 inner_call_handler = {
-    "ise_getPReps": get_preps
+    "ise_getPRepList": get_preps
 }
 
 
