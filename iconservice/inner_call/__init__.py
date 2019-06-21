79a8b12d48c0948e5546ca93ec4a74bea90f4755
inwonkim-icon <37284520+inwonkim@users.noreply.github.com>
2019-06-21 11:06:43 +0900

    [Is-509] Use make_prep_tx_result when inner-call called. (#185)
    * Add MQ message
    
    - write call method in inner_service
    - write getting PReps method
    
    * Change the return value of get_preps to follow the api specification
    
    * If prep does not exist, set rootHash to an empty string
    
    * Separate method of saving term from making prep transaction result
    
    - Amends to get preps, not from the term instance but the context
    
    * Use make_prep_tx_result when inner-call called

--- iconservice/inner_call/__init__.py
+++ iconservice/inner_call/__init__.py
@@ -11,51 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import hashlib
-from typing import TYPE_CHECKING
 
-from ..icon_constant import PREP_COUNT
+from ..base.type_converter import TypeConverter
 from ..iconscore.icon_score_context import IconScoreContext
-
-if TYPE_CHECKING:
-    from ..base.block import Block
-    from ..prep.data.prep import PRep
-    from ..prep.data.prep_container import PRepContainer
-
-
-def get_preps_root_hash(prep_id_list: list) -> bytes:
-    return hashlib.sha3_256(b''.join(prep_id_list)).digest()
 
 
 def get_preps(context: IconScoreContext):
-    preps: 'PRepContainer' = context.engine.prep.preps
-    prep_result = []
-    prep_ids_in_bytes = []
+    preps: 'dict' = context.engine.prep.make_prep_tx_result()
+    if preps is None:
+        preps = {}
 
-    for prep in preps:
-        p_rep: 'PRep' = context.engine.prep.preps.get(prep.address)
-        data = {
-            "id": str(p_rep.address),
-            "publicKey": f"0x{bytes.hex(p_rep.public_key)}",
-            "p2pEndPoint": p_rep.p2p_end_point
-        }
-        prep_result.append(data)
-        prep_ids_in_bytes.append(p_rep.address.to_bytes())
+    block = context.storage.icx.last_block
+    preps['blockHeight'] = 0 if block is None else block
+    preps.pop('state', None)
 
-        if len(prep_result) == PREP_COUNT:
-            break
-
-    block: 'Block' = context.storage.icx.last_block
+    TypeConverter.convert_type_reverse(preps)
     result = {
-        "result": {
-            "blockHeight": 0 if block is None else block.height,
-            "preps": prep_result,
-            "rootHash": ""
-        }
+        "result": preps
     }
-    if prep_result:
-        root_hash = get_preps_root_hash(prep_ids_in_bytes)
-        result['rootHash'] = f"0x{root_hash.hex()}"
 
     return result
 
