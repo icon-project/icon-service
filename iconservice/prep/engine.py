a5adeebbee0f008efeef2e737efbe8e6844e29f5
inwonkim <inwonkim@icon.foundation>
2019-06-25 17:22:47 +0900

    Refactor inner-call.
    * extract getting prep logic from `make_prep_tx_result` method
    * fix some wrong codes
    * rename method(get_preps to get_main_preps)

--- iconservice/prep/engine.py
+++ iconservice/prep/engine.py
@@ -29,7 +29,6 @@ from ..icon_constant import PrepResultState, IISS_MIN_IREP
 from ..iconscore.icon_score_event_log import EventLogEmitter
 from ..icx.storage import Intent
 from ..iiss.reward_calc import RewardCalcDataCreator
-from ..precommit_data_manager import PrecommitData
 
 if TYPE_CHECKING:
     from . import PRepStorage
@@ -167,23 +166,29 @@ class Engine(EngineBase):
         return self.term.end_block_height == context.block.height
 
     def make_prep_tx_result(self) -> Optional[dict]:
+        prep_as_dict = self.get_main_preps_in_term()
+        if prep_as_dict:
+            prep_as_dict['irep'] = self.term.irep
+            prep_as_dict['state'] = PrepResultState.NORMAL.value
+            return prep_as_dict
+        return None
+
+    def get_main_preps_in_term(self) -> Optional[dict]:
         main_preps = self.term.main_preps
         prep_as_dict = None
         if len(main_preps) > 0:
             prep_as_dict = OrderedDict()
             preps_as_list = []
-            preps_as_list_for_roothash = []
-            for prep in self.term.main_preps:
+            prep_addresses_for_roothash = b''
+            for prep in main_preps:
                 prep_info_as_dict = OrderedDict()
                 prep_info_as_dict[ConstantKeys.PREP_ID] = prep.address
                 prep_info_as_dict[ConstantKeys.PUBLIC_KEY] = prep.public_key
                 prep_info_as_dict[ConstantKeys.P2P_END_POINT] = prep.p2p_end_point
                 preps_as_list.append(prep_info_as_dict)
-                preps_as_list_for_roothash.extend(
-                    [prep.address.to_bytes(), prep.public_key, prep.p2p_end_point.encode()])
+                prep_addresses_for_roothash += prep.address.to_bytes()
             prep_as_dict["preps"] = preps_as_list
-            prep_as_dict["state"] = PrepResultState.NORMAL.value
-            prep_as_dict["rootHash"] = hashlib.sha3_256(b'|'.join(preps_as_list_for_roothash)).digest()
+            prep_as_dict["rootHash"] = hashlib.sha3_256(prep_addresses_for_roothash).digest()
         return prep_as_dict
 
     def save_term(self, context: 'IconScoreContext'):
