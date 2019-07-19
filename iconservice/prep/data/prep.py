050c2e1df9cfee5c6221f337214bff3d0af8bd94
Chiwon Cho <goldworm@icon.foundation>
2019-07-19 19:05:58 +0900

    IS-637: Add stake field to PRep

--- iconservice/prep/data/prep.py
+++ iconservice/prep/data/prep.py
@@ -87,6 +87,7 @@ class PRep(Sortable):
             public_key: bytes = b"",
             irep: int = 0,
             irep_block_height: int = 0,
+            stake: int = 0,
             delegated: int = 0,
             block_height: int = 0,
             tx_index: int = 0,
@@ -111,6 +112,7 @@ class PRep(Sortable):
         :param p2p_endpoint:
         :param irep:
         :param irep_block_height:
+        :param stake:
         :param delegated:
         :param block_height:
         :param tx_index:
@@ -123,7 +125,7 @@ class PRep(Sortable):
         # flags
         self._flags: 'PRepFlag' = flags
 
-        # The delegated amount retrieved from account
+        self._stake: int = stake
         self._delegated: int = delegated
 
         # status
@@ -230,11 +232,22 @@ class PRep(Sortable):
         return self._address
 
     @property
+    def stake(self) -> int:
+        return self._stake
+
+    @stake.setter
+    def stake(self, value: int):
+        assert value >= 0
+        self._check_access_permission()
+        self._stake = value
+
+    @property
     def delegated(self) -> int:
         return self._delegated
 
     @delegated.setter
     def delegated(self, value: int):
+        assert value >= 0
         self._check_access_permission()
         self._delegated = value
 
@@ -355,6 +368,7 @@ class PRep(Sortable):
             self.grade.value,
             self.name,
             self.country,
+            self.city,
             self.email,
             self.website,
             self.details,
@@ -432,12 +446,18 @@ class PRep(Sortable):
         )
 
     def to_dict(self, dict_type: 'PRepDictType') -> dict:
+        """Returns the P-Rep information in dict format
+
+        :param dict_type: FULL(getPRep), ABRIDGED(getPRepList)
+        :return:
+        """
         data = {
             "status": self._status.value,
             "grade": self.grade.value,
             "name": self.name,
             "country": self.country,
             "city": self.city,
+            "stake": self._stake,
             "delegated": self._delegated,
             "totalBlocks": self._total_blocks,
             "validatedBlocks": self._validated_blocks
