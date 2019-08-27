e5d85a0fb9ebc27b78e67fb3763b8e1bb7ad17eb
Chiwon Cho <goldworm@icon.foundation>
2019-08-27 17:18:02 +0900

    IS-766: Fix msgId conflict on RewardCalcProxy

---
+++ tests/iiss/ipc/test_message.py
@@ -0,0 +1,25 @@
+import unittest
+
+from iconservice.iiss.reward_calc.ipc import message
+
+
+class TestMessage(unittest.TestCase):
+    def test_get_next_id(self):
+        assert message._next_msg_id == 1
+
+        msg_id: int = message._get_next_id()
+        assert msg_id == 1
+        assert message._next_msg_id == 2
+
+        msg_id: int = message._get_next_id()
+        assert msg_id == 2
+        assert message._next_msg_id == 3
+
+        message._next_msg_id = 0xffffffff
+        msg_id: int = message._get_next_id()
+        assert msg_id == 0xffffffff
+        assert message._next_msg_id == 1
+
+        msg_id: int = message._get_next_id()
+        assert msg_id == 1
+        assert message._next_msg_id == 2
