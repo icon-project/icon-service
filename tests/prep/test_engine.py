5bb3b0c08cc84689c101b65c8bf08fcb8289f029
Chiwon Cho <goldworm@icon.foundation>
2019-09-13 02:48:57 +0900

    Fix an unittest error in test_engine.py
    * Add icx_to_loop()

--- tests/prep/test_engine.py
+++ tests/prep/test_engine.py
@@ -13,53 +13,93 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import random
+import os
 import unittest
-from typing import List, Tuple
-from unittest import mock
+from unittest.mock import Mock
 
 from iconservice.base.address import AddressPrefix, Address
+from iconservice.base.block import Block
 from iconservice.icon_constant import PREP_MAIN_PREPS, PREP_MAIN_AND_SUB_PREPS
-from iconservice.icon_constant import PRepGrade
+from iconservice.icon_constant import PRepGrade, IconScoreContextType
+from iconservice.iconscore.icon_score_context import IconScoreContext, IconScoreContextFactory
+from iconservice.iconscore.icon_score_step import IconScoreStepCounterFactory
 from iconservice.prep import PRepEngine
-from iconservice.prep.data import PRep, PRepContainer
+from iconservice.prep.data import PRep, PRepContainer, Term
+from iconservice.utils import icx_to_loop
+
+
+def _create_context() -> 'IconScoreContext':
+    step_counter = Mock(spec=IconScoreStepCounterFactory)
+    context_factory = IconScoreContextFactory(step_counter)
+    block = Block(block_height=100,
+                  block_hash=os.urandom(32),
+                  timestamp=0,
+                  prev_hash=os.urandom(32),
+                  cumulative_fee=0)
+
+    return context_factory.create(IconScoreContextType.INVOKE, block)
+
+
+def _create_term(total_supply: int, total_delegated: int):
+    return Term(sequence=0,
+                start_block_height=100,
+                period=43120,
+                irep=icx_to_loop(50000),
+                total_supply=total_supply,
+                total_delegated=total_delegated)
 
 
 class TestEngine(unittest.TestCase):
     def setUp(self) -> None:
         size = 200
-        self.new_preps = PRepContainer()
+        total_delegated = 0
 
-        # Create 200 dummy preps
+        preps = PRepContainer()
+
+        # Create dummy preps
         for i in range(size):
             address = Address.from_prefix_and_int(AddressPrefix.EOA, i)
-            prep = PRep(address, block_height=i, irep_block_height=i)
-            self.assertEqual(PRepGrade.CANDIDATE, prep.grade)
-            self.assertEqual(0, prep.delegated)
+            delegated = icx_to_loop(1000 - i)
+            total_delegated += delegated
 
-            self.new_preps.add(prep)
+            prep = PRep(address,
+                        block_height=i,
+                        delegated=delegated)
+            prep.freeze()
+
+            assert prep.grade == PRepGrade.CANDIDATE
+            assert prep.delegated == delegated
+            assert prep.block_height == i
+            preps.add(prep)
+
+        preps.freeze()
+
+        prep_engine = PRepEngine()
+        prep_engine.preps = preps
+
+        IconScoreContext.engine = Mock()
+        IconScoreContext.engine.prep = prep_engine
+        IconScoreContext.storage = Mock()
+        IconScoreContext.storage.prep = Mock()
+
+        context = _create_context()
+        context.main_prep_count = PREP_MAIN_PREPS
+        context.main_and_sub_prep_count = PREP_MAIN_AND_SUB_PREPS
+
+        self.context = context
+        self.total_supply = icx_to_loop(8_046_000)
+        self.total_delegated = total_delegated
 
     def tearDown(self) -> None:
         self.new_preps = None
 
     def test__update_prep_grades_1(self):
-        old_prep_list = []
-        delegated_list: List[Tuple[int, int]] = []
-        new_preps: 'PRepContainer' = self.new_preps
-
-        term = mock.MagicMock()
-        term.preps = []
-
-        context = mock.MagicMock()
-        context.main_prep_count = PREP_MAIN_PREPS
-        context.main_and_sub_prep_count = PREP_MAIN_AND_SUB_PREPS
-        context.preps = new_preps
-        context.term = term
+        context = self.context
 
         # Case0: Network has just decentralized without any delegation
-        PRepEngine._update_prep_grades_on_term_ended(context)
+        context.engine.prep._update_prep_grades_on_term_ended(context)
 
-        for i, prep in enumerate(new_preps):
+        for i, prep in enumerate(context.preps):
             if i < PREP_MAIN_PREPS:
                 self.assertEqual(PRepGrade.MAIN, prep.grade)
             elif i < PREP_MAIN_AND_SUB_PREPS:
@@ -67,42 +107,37 @@ class TestEngine(unittest.TestCase):
             else:
                 self.assertEqual(PRepGrade.CANDIDATE, prep.grade)
 
-        # Case1: Sort preps in descending order by prep.order()
-        for i, prep in enumerate(new_preps):
-            delegated: int = random.randint(0, 10_000)
-            delegated_list.append((-delegated, i))
+        term = _create_term(self.total_supply, self.total_delegated)
+        term.set_preps(context.preps, context.main_prep_count, context.main_and_sub_prep_count)
+        term.freeze()
 
-            old_prep_list.append(prep)
-            new_preps.remove(prep.address)
-            prep.delegated = delegated
-            new_preps.add(prep)
+        context.engine.prep.term = term
+        context.engine.prep.preps = context.preps
+        context.engine.prep.preps.freeze()
 
-        # Expected list in descending order by (delegated, block_height, tx_index)
-        delegated_list.sort()
+        context.preps = context.engine.prep.preps.copy(mutable=True)
 
-        for i in range(200):
-            prep = old_prep_list[i]
-            prep2 = new_preps.get_by_address(prep.address)
-            self.assertEqual(id(prep), id(prep2))
+        for i in range(100):
+            prep = context.preps.get_by_index(i + context.main_and_sub_prep_count)
+            dirty_prep = prep.copy()
+            dirty_prep.delegated = icx_to_loop(5000) - i
+            context.put_dirty_prep(dirty_prep)
 
-        PRepEngine._update_prep_grades_on_term_ended(main_prep_count=PREP_MAIN_PREPS,
-                                                     main_and_sub_prep_count=PREP_MAIN_AND_SUB_PREPS,
-                                                     old_preps=old_prep_list,
-                                                     new_preps=new_preps)
+        context.update_dirty_prep_batch()
 
-        i = 0
-        for delegated, index in delegated_list:
-            prep: 'PRep' = new_preps.get_by_index(i)
-            prep_in_list: 'PRep' = old_prep_list[index]
+        context.engine.prep._update_prep_grades_on_term_ended(context)
 
-            self.assertEqual(abs(delegated), prep.delegated)
-            self.assertEqual(prep_in_list.address, prep.address)
-
+        for i, prep in enumerate(context.preps):
             if i < PREP_MAIN_PREPS:
-                self.assertEqual(PRepGrade.MAIN, prep.grade)
+                assert prep.grade == PRepGrade.MAIN
+                assert prep.delegated == icx_to_loop(5000) - i
             elif i < PREP_MAIN_AND_SUB_PREPS:
                 self.assertEqual(PRepGrade.SUB, prep.grade)
+                assert prep.delegated == icx_to_loop(5000) - i
             else:
                 self.assertEqual(PRepGrade.CANDIDATE, prep.grade)
+                assert prep.delegated == icx_to_loop(1000 - i + context.main_and_sub_prep_count)
 
-            i += 1
+        for i, prep_snapshot in enumerate(term.preps):
+            prep = context.preps.get_by_index(i + context.main_and_sub_prep_count)
+            assert prep_snapshot.address == prep.address
