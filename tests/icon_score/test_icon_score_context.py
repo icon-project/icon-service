d6f7885e64b326ab19cff58eaf8ad65bd7eb693f
Chiwon Cho <goldworm@icon.foundation>
2020-01-14 19:47:09 +0900

    Add unittests for IconScoreContext code
    * Test IconScoreContext.update_dirty_prep_batch()

--- tests/icon_score/test_icon_score_context.py
+++ tests/icon_score/test_icon_score_context.py
@@ -17,7 +17,8 @@ import random
 import unittest
 from unittest.mock import Mock
 
-from iconservice.icon_constant import IconScoreContextType, PRepFlag, TermFlag, Revision, PRepGrade
+from iconservice.icon_constant import IconScoreContextType, PRepFlag, TermFlag, Revision, PRepGrade, PenaltyReason, \
+    PRepStatus
 from iconservice.iconscore.icon_score_context import IconScoreContext, IconScoreContextFactory
 from iconservice.iconscore.icon_score_step import IconScoreStepCounterFactory
 from iconservice.prep.data import Term, PRep
@@ -27,9 +28,18 @@ from iconservice.utils import icx_to_loop
 from .. import utils
 
 
+def _impose_penalty_on_prep(prep: 'PRep', penalty: 'PenaltyReason'):
+    assert not prep.is_frozen()
+
+    prep.penalty = penalty
+    prep.grade = PRepGrade.CANDIDATE
+    prep.status = PRepStatus.ACTIVE if penalty == PenaltyReason.BLOCK_VALIDATION else PRepStatus.DISQUALIFIED
+
+
 class TestIconScoreContext(unittest.TestCase):
     MAIN_PREPS = 22
     ELECTED_PREPS = 100
+    TOTAL_PREPS = 110
     TERM_PERIOD = 43120
 
     def setUp(self) -> None:
@@ -37,7 +47,7 @@ class TestIconScoreContext(unittest.TestCase):
         IconScoreContext.storage = Mock()
 
         preps = utils.create_dummy_preps(
-            size=self.ELECTED_PREPS + 10, main_preps=self.MAIN_PREPS, elected_preps=self.ELECTED_PREPS)
+            size=self.TOTAL_PREPS, main_preps=self.MAIN_PREPS, elected_preps=self.ELECTED_PREPS)
         preps.freeze()
         assert preps.is_frozen()
         assert not preps.is_dirty()
@@ -150,3 +160,163 @@ class TestIconScoreContext(unittest.TestCase):
         assert context.preps.is_dirty()
         assert context.preps.get_by_index(sub_index) == dirty_sub_prep
         assert context.preps.get_by_index(candidate_index) == dirty_prep_candidate
+
+    def test_update_dirty_prep_batch_with_penalized_main_prep(self):
+        block = utils.create_dummy_block()
+        context: 'IconScoreContext' = self.context_factory.create(IconScoreContextType.INVOKE, block)
+        context.revision = Revision.REALTIME_P2P_ENDPOINT_UPDATE.value
+        assert isinstance(context.preps, PRepContainer)
+        assert context.preps.is_frozen()
+        assert context.term.is_frozen()
+
+        penalties = [
+            PenaltyReason.LOW_PRODUCTIVITY,
+            PenaltyReason.PREP_DISQUALIFICATION,
+            PenaltyReason.BLOCK_VALIDATION
+        ]
+
+        sub_prep_count = self.ELECTED_PREPS - self.MAIN_PREPS
+
+        # Check for 3 penalties
+        for i, penalty in enumerate(penalties):
+            # Choose a main P-Rep
+            address = context.term.main_preps[i].address
+
+            # Duplicate a main P-Rep indicated by address to dirty_prep
+            dirty_prep: 'PRep' = context.get_prep(address, mutable=True)
+            assert dirty_prep.address == address
+            assert dirty_prep.grade == PRepGrade.MAIN
+            assert not dirty_prep.is_frozen()
+            assert not dirty_prep.is_dirty()
+
+            # Impose low productivity penalty on the main P-Rep chosen above
+            _impose_penalty_on_prep(dirty_prep, penalty)
+            assert dirty_prep.is_flags_on(PRepFlag.GRADE | PRepFlag.PENALTY)
+            assert dirty_prep.is_flags_on(PRepFlag.STATUS) == (penalty != PenaltyReason.BLOCK_VALIDATION)
+
+            context.put_dirty_prep(dirty_prep)
+            context.update_dirty_prep_batch()
+
+            # context.preps and context.term should be mutable and dirty
+            # because main P-Rep substitution happened by penalty
+            assert not context.preps.is_frozen()
+            assert not context.term.is_frozen()
+            assert context.preps.is_dirty()
+            assert context.term.is_dirty()
+
+            # The penalized main P-Rep should be replaced with the first sub P-Rep
+            assert not context.term.is_main_prep(dirty_prep.address)
+            assert len(context.term.main_preps) == self.MAIN_PREPS
+            assert len(context.term.sub_preps) == sub_prep_count - 1
+            sub_prep_count -= 1
+
+            assert id(dirty_prep) == id(context.preps.get_by_address(address))
+
+            address = context.term.main_preps[i].address
+            new_main_prep: 'PRep' = context.preps.get_by_address(address)
+
+            # The grade update for a new main P-Rep will be batched
+            # in iconservice.prep.engine.Engine._update_prep_grades()
+            assert new_main_prep.grade == PRepGrade.SUB
+            assert not new_main_prep.is_dirty()
+
+    def test_update_dirty_prep_batch_by_elected_prep_unregistration(self):
+        items = [
+            (random.randint(0, self.MAIN_PREPS - 1), PRepGrade.MAIN),
+            (random.randint(self.MAIN_PREPS, self.ELECTED_PREPS - 1), PRepGrade.SUB),
+        ]
+
+        for index, grade in items:
+            block = utils.create_dummy_block()
+            context: 'IconScoreContext' = self.context_factory.create(IconScoreContextType.INVOKE, block)
+            context.revision = Revision.REALTIME_P2P_ENDPOINT_UPDATE.value
+            assert isinstance(context.preps, PRepContainer)
+            assert context.preps.is_frozen()
+            assert context.term.is_frozen()
+
+            prep: 'PRep' = context.preps.get_by_index(index)
+            assert prep.grade == grade
+
+            # Duplicate the P-Rep specified by address to dirty_prep
+            dirty_prep = context.get_prep(prep.address, mutable=True)
+            assert dirty_prep.address == prep.address
+            assert dirty_prep.grade == grade
+            assert not dirty_prep.is_frozen()
+            assert not dirty_prep.is_dirty()
+
+            # Unregister the P-Rep
+            dirty_prep.status = PRepStatus.UNREGISTERED
+            dirty_prep.grade = PRepGrade.CANDIDATE
+            dirty_prep.penalty = PenaltyReason.NONE
+            # The status and grade of the P-Rep are changed
+            assert dirty_prep.is_flags_on(PRepFlag.STATUS | PRepFlag.GRADE)
+            # The penalty of the P-Rep is not changed
+            assert not dirty_prep.is_flags_on(PRepFlag.PENALTY)
+
+            context.put_dirty_prep(dirty_prep)
+            context.update_dirty_prep_batch()
+
+            # context.preps and context.term should be mutable and dirty
+            assert not context.preps.is_frozen()
+            assert not context.term.is_frozen()
+            assert context.preps.is_dirty()
+            assert context.term.is_dirty()
+
+            assert dirty_prep.address not in context.term
+            assert dirty_prep == context.preps.get_by_address(dirty_prep.address)
+
+    def test_update_dirty_prep_batch_by_prep_candidate_unregistration(self):
+        for i in range(3):
+            index = self.ELECTED_PREPS + i
+            grade = PRepGrade.CANDIDATE
+
+            block = utils.create_dummy_block()
+            context: 'IconScoreContext' = self.context_factory.create(IconScoreContextType.INVOKE, block)
+            context.revision = Revision.REALTIME_P2P_ENDPOINT_UPDATE.value
+            assert isinstance(context.preps, PRepContainer)
+            assert context.preps.is_frozen()
+            assert context.term.is_frozen()
+
+            old_active_prep_count: int = context.preps.size(active_prep_only=True)
+            old_total_prep_count: int = context.preps.size(active_prep_only=False)  # active + inactive
+            # There is no inactive P-Rep at this moment
+            assert old_active_prep_count == old_total_prep_count
+
+            prep: 'PRep' = context.preps.get_by_index(index)
+            assert prep.grade == grade
+
+            # Duplicate the P-Rep specified by address to dirty_prep
+            dirty_prep = context.get_prep(prep.address, mutable=True)
+            assert dirty_prep.address == prep.address
+            assert dirty_prep.grade == grade
+            assert not dirty_prep.is_frozen()
+            assert not dirty_prep.is_dirty()
+
+            # Unregister the P-Rep
+            dirty_prep.status = PRepStatus.UNREGISTERED
+            dirty_prep.grade = PRepGrade.CANDIDATE
+            dirty_prep.penalty = PenaltyReason.NONE
+            # The status of the P-Rep is changed
+            assert dirty_prep.is_flags_on(PRepFlag.STATUS)
+            # The penalty and grade of the P-Rep are not changed
+            assert not dirty_prep.is_flags_on(PRepFlag.PENALTY | PRepFlag.GRADE)
+
+            # A P-Rep candidate unregistration will be applied to context
+            context.put_dirty_prep(dirty_prep)
+            context.update_dirty_prep_batch()
+
+            # context.preps should become mutable and dirty
+            assert not context.preps.is_frozen()
+            assert context.preps.is_dirty()
+
+            # As the unregistered P-Rep is not an elected P-Rep, nothing happens to context.term
+            assert context.term.is_frozen()
+            assert not context.term.is_dirty()
+
+            # A P-Rep candidate unregistration do not affect context.term
+            assert dirty_prep.address not in context.term
+            assert dirty_prep == context.preps.get_by_address(dirty_prep.address)
+
+            # The number of active P-Reps will be decreased by one compared to the previous one
+            assert context.preps.size(active_prep_only=True) == old_active_prep_count - 1
+            assert context.preps.size(active_prep_only=False) == old_total_prep_count
