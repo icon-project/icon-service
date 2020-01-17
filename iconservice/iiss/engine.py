630bbf73fa58bbf1967d62654da6a58c3d62414f
soobokjin <soobokjin@icon.foundation>
2020-01-17 14:39:00 +0900

    Do not put the bp info on the first sequence of the term (i.e. Decentralization)

--- iconservice/iiss/engine.py
+++ iconservice/iiss/engine.py
@@ -731,10 +731,11 @@ class Engine(EngineBase):
         if not context.is_decentralized():
             return rc_state_hash
 
-        self.put_block_produce_info_to_rc_db(context,
-                                             context.rc_block_batch,
-                                             prev_block_generator,
-                                             prev_block_votes)
+        if not context.is_the_first_block_on_decentralization():
+            self.put_block_produce_info_to_rc_db(context,
+                                                 context.rc_block_batch,
+                                                 prev_block_generator,
+                                                 prev_block_votes)
 
         start_term_block: int = context.engine.prep.term.start_block_height
         # New P-Rep Term is started
@@ -861,7 +862,7 @@ class Engine(EngineBase):
         :param prev_block_votes:
         :return:
         """
-        assert context.is_decentralized()
+        assert context.is_decentralized() and not context.is_the_first_block_on_decentralization()
         if prev_block_generator is None or prev_block_votes is None:
             return
 
