2cef02f2667cbc38ea1b6667fdc16294e817412b
soobokjin <soobokjin@icon.foundation>
2020-02-11 18:53:55 +0900

    Change the way of setting system value

--- iconservice/system/value.py
+++ iconservice/system/value.py
@@ -48,72 +48,41 @@ class SystemValue:
         self._import_white_list: list = import_white_list
         self._import_white_list_keys: str = import_white_list_key
 
-        self._set_value_mapper = {
-            SystemValueType.STEP_PRICE: self._set_step_price,
-            SystemValueType.STEP_COSTS: self._set_step_costs,
-            SystemValueType.MAX_STEP_LIMITS: self._set_max_step_limits,
-            SystemValueType.REVISION_CODE: self._set_revision_code,
-            SystemValueType.REVISION_NAME: self._set_revision_name,
-            SystemValueType.SCORE_BLACK_LIST: self._set_score_black_list,
-            SystemValueType.IMPORT_WHITE_LIST: self._set_import_white_list,
-            SystemValueType.IMPORT_WHITE_LIST_KEYS: self._set_import_white_list_keys,
-        }
+    @property
+    def is_migrated(self):
+        return self._is_migrated
 
     @property
     def step_price(self):
         return self._step_price
 
-    def _set_step_price(self, step_price: int):
-        self._step_price: int = step_price
-
     @property
     def step_costs(self):
         return self._step_costs
-
-    def _set_step_costs(self, step_costs: int):
-        self._step_costs: int = step_costs
 
     @property
     def max_step_limits(self):
         return self._max_step_limits
 
-    def _set_max_step_limits(self, max_step_limits: dict):
-        self._max_step_limits: dict = max_step_limits
-
     @property
     def revision_code(self):
         return self._revision_code
-
-    def _set_revision_code(self, revision_code: int):
-        self._revision_code: int = revision_code
 
     @property
     def revision_name(self):
         return self._revision_name
 
-    def _set_revision_name(self, revision_name: str):
-        self._revision_name: str = revision_name
-
     @property
     def score_black_list(self):
         return self._score_black_list
-
-    def _set_score_black_list(self, score_black_list: list):
-        self._score_black_list: list = score_black_list
 
     @property
     def import_white_list(self):
         return self._import_white_list
 
-    def _set_import_white_list(self, import_white_list: list):
-        self._import_white_list: list = import_white_list
-
     @property
     def import_white_list_keys(self):
         return self._import_white_list_keys
-
-    def _set_import_white_list_keys(self, import_white_list_keys: str):
-        self._import_white_list_keys: str = import_white_list_keys
 
     def create_step_counter(self,
                             context_type: 'IconScoreContextType',
@@ -125,7 +94,7 @@ class SystemValue:
 
         return IconScoreStepCounter(step_price, step_costs, max_step_limit, step_trace_flag)
 
-    def migrates(self, context: 'IconScoreContext', data: Dict['SystemValueType', Any]):
+    def migrate(self, context: 'IconScoreContext', data: Dict['SystemValueType', Any]):
         """
         Migrates governance variablie from Governance score to Governance Value.
         It will be called when updating governance score to version "".
@@ -140,28 +109,45 @@ class SystemValue:
         self._is_migrated = True
 
     def _set_value(self, value_type: 'SystemValueType', value: Any):
-        self._set_value_mapper[value_type](value)
+        if value_type == SystemValueType.STEP_PRICE:
+            self._step_price = value
+        elif value_type == SystemValueType.STEP_COSTS:
+            self._step_costs = value
+        elif value_type == SystemValueType.MAX_STEP_LIMITS:
+            self._max_step_limits = value
+        elif value_type == SystemValueType.REVISION_CODE:
+            self._revision_code = value
+        elif value_type == SystemValueType.SCORE_BLACK_LIST:
+            self._score_black_list = value
+        elif value_type == SystemValueType.IMPORT_WHITE_LIST:
+            self._import_white_list = value
+        elif value_type == SystemValueType.IMPORT_WHITE_LIST_KEYS:
+            self._import_white_list_keys= value
+        else:
+            raise ValueError(f"Invalid value type: {value_type.name}")
 
-    def set_value_before_migration(self):
+    def set_value_before_migration(self, value_type: 'SystemValueType', value: Any):
         """
-
+        Set value on system value instance before migration.
+        Update entire values simultaneously as it is hard to know which data has been changed.
+        Only Iconservice can set values before migration
         :return:
         """
         assert not self._is_migrated
-        # Update member variables before migrations.
-        # Must not put data to DB before migrations (only update member variables)
-        # get all data from the governance score
-        # update member variables
+        assert isinstance(value_type, SystemValueType)
+        # Update member variables
+        self._set_value(value_type, value)
 
     def set_value_after_migration(self, context: 'IconScoreContext', value_type: 'SystemValueType', value: Any):
         """
-
+        Set values on system value and put these into DB.
+        Only Governance Score can set values after migration.
         :param context:
         :param value_type:
         :param value:
         :return:
         """
-        # Todo: Only GS can access. IS can not call directly
+        # Todo: Only GS can access. IS can not call directly (Inspect caller)
         assert self._is_migrated
         assert isinstance(value_type, SystemValueType)
         # Update member variables
