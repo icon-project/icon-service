8eb1c2ab5d8fba2227838f72f16bb2edc47f4e8d
leeheonseung <mycom22@icon.foundation>
2020-02-27 12:03:46 +0900

    Revert "tt"
    This reverts commit bdbd243af0f656efe8d9e0cb2c1160e5bc9af370.

--- iconservice/iconscore/icon_container_db.py
+++ iconservice/iconscore/icon_container_db.py
@@ -39,27 +39,27 @@ def get_encoded_key(key: V) -> bytes:
 
 class ContainerUtil(object):
 
-    @classmethod
-    def create_db_prefix(cls, cls_obj, var_key: K) -> bytes:
+    @staticmethod
+    def create_db_prefix(cls, var_key: K) -> bytes:
         """Create a prefix used
         as a parameter of IconScoreDatabase.get_sub_db()
 
-        :param cls_obj: ArrayDB, DictDB, VarDB
+        :param cls: ArrayDB, DictDB, VarDB
         :param var_key:
         :return:
         """
-        if cls_obj == ArrayDB:
+        if cls == ArrayDB:
             container_id = ARRAY_DB_ID
-        elif cls_obj == DictDB:
+        elif cls == DictDB:
             container_id = DICT_DB_ID
         else:
-            raise InvalidParamsException(f'Unsupported container class: {cls_obj}')
+            raise InvalidParamsException(f'Unsupported container class: {cls}')
 
         encoded_key: bytes = get_encoded_key(var_key)
         return b'|'.join([container_id, encoded_key])
 
-    @classmethod
-    def encode_key(cls, key: K) -> bytes:
+    @staticmethod
+    def encode_key(key: K) -> bytes:
         """Create a key passed to IconScoreDatabase
 
         :param key:
@@ -80,8 +80,8 @@ class ContainerUtil(object):
             raise InvalidParamsException(f'Unsupported key type: {type(key)}')
         return bytes_key
 
-    @classmethod
-    def encode_value(cls, value: V) -> bytes:
+    @staticmethod
+    def encode_value(value: V) -> bytes:
         if isinstance(value, int):
             byte_value = int_to_bytes(value)
         elif isinstance(value, str):
@@ -96,8 +96,8 @@ class ContainerUtil(object):
             raise InvalidParamsException(f'Unsupported value type: {type(value)}')
         return byte_value
 
-    @classmethod
-    def decode_object(cls, value: bytes, value_type: type) -> Optional[Union[K, V]]:
+    @staticmethod
+    def decode_object(value: bytes, value_type: type) -> Optional[Union[K, V]]:
         if value is None:
             return get_default_value(value_type)
 
@@ -114,45 +114,45 @@ class ContainerUtil(object):
             obj_value = value
         return obj_value
 
-    @classmethod
-    def remove_prefix_from_iters(cls, iter_items: iter) -> iter:
-        return ((cls.__remove_prefix_from_key(key), value) for key, value in iter_items)
+    @staticmethod
+    def remove_prefix_from_iters(iter_items: iter) -> iter:
+        return ((ContainerUtil.__remove_prefix_from_key(key), value) for key, value in iter_items)
 
-    @classmethod
-    def __remove_prefix_from_key(cls, key_from_bytes: bytes) -> bytes:
+    @staticmethod
+    def __remove_prefix_from_key(key_from_bytes: bytes) -> bytes:
         return key_from_bytes[:-1]
 
-    @classmethod
-    def put_to_db(cls, db: 'IconScoreDatabase', db_key: str, container: iter) -> None:
-        sub_db = db.get_sub_db(cls.encode_key(db_key))
+    @staticmethod
+    def put_to_db(db: 'IconScoreDatabase', db_key: str, container: iter) -> None:
+        sub_db = db.get_sub_db(ContainerUtil.encode_key(db_key))
         if isinstance(container, dict):
-            cls.__put_to_db_internal(sub_db, container.items())
+            ContainerUtil.__put_to_db_internal(sub_db, container.items())
         elif isinstance(container, (list, set, tuple)):
-            cls.__put_to_db_internal(sub_db, enumerate(container))
+            ContainerUtil.__put_to_db_internal(sub_db, enumerate(container))
 
-    @classmethod
-    def get_from_db(cls, db: 'IconScoreDatabase', db_key: str, *args, value_type: type) -> Optional[K]:
-        sub_db = db.get_sub_db(cls.encode_key(db_key))
+    @staticmethod
+    def get_from_db(db: 'IconScoreDatabase', db_key: str, *args, value_type: type) -> Optional[K]:
+        sub_db = db.get_sub_db(ContainerUtil.encode_key(db_key))
         *args, last_arg = args
         for arg in args:
-            sub_db = sub_db.get_sub_db(cls.encode_key(arg))
+            sub_db = sub_db.get_sub_db(ContainerUtil.encode_key(arg))
 
-        byte_key = sub_db.get(cls.encode_key(last_arg))
+        byte_key = sub_db.get(ContainerUtil.encode_key(last_arg))
         if byte_key is None:
             return get_default_value(value_type)
-        return cls.decode_object(byte_key, value_type)
+        return ContainerUtil.decode_object(byte_key, value_type)
 
-    @classmethod
-    def __put_to_db_internal(cls, db: Union['IconScoreDatabase', 'IconScoreSubDatabase'], iters: iter) -> None:
+    @staticmethod
+    def __put_to_db_internal(db: Union['IconScoreDatabase', 'IconScoreSubDatabase'], iters: iter) -> None:
         for key, value in iters:
-            sub_db = db.get_sub_db(cls.encode_key(key))
+            sub_db = db.get_sub_db(ContainerUtil.encode_key(key))
             if isinstance(value, dict):
-                cls.__put_to_db_internal(sub_db, value.items())
+                ContainerUtil.__put_to_db_internal(sub_db, value.items())
             elif isinstance(value, (list, set, tuple)):
-                cls.__put_to_db_internal(sub_db, enumerate(value))
+                ContainerUtil.__put_to_db_internal(sub_db, enumerate(value))
             else:
-                db_key = cls.encode_key(key)
-                db_value = cls.encode_value(value)
+                db_key = ContainerUtil.encode_key(key)
+                db_value = ContainerUtil.encode_value(value)
                 db.put(db_key, db_value)
 
 
@@ -270,15 +270,18 @@ class ArrayDB(object):
         return self[index]
 
     def __get_size(self) -> int:
-        return self.__legacy_size
+        if self.__is_defective_revision():
+            return self.__legacy_size
+        else:
+            return self.__get_size_from_db()
 
     def __get_size_from_db(self) -> int:
-        return ContainerUtil.decode_object(self._db.get(self.__SIZE_BYTE_KEY), int)
+        return ContainerUtil.decode_object(self._db.get(ArrayDB.__SIZE_BYTE_KEY), int)
 
     def __set_size(self, size: int) -> None:
         self.__legacy_size = size
         byte_value = ContainerUtil.encode_value(size)
-        self._db.put(self.__SIZE_BYTE_KEY, byte_value)
+        self._db.put(ArrayDB.__SIZE_BYTE_KEY, byte_value)
 
     def __put(self, index: int, value: V) -> None:
         byte_value = ContainerUtil.encode_value(value)
@@ -306,7 +309,7 @@ class ArrayDB(object):
             raise InvalidParamsException('ArrayDB out of index')
 
     def __getitem__(self, index: int) -> V:
-        return self._get(self._db, self.__get_size(), index, self.__value_type)
+        return ArrayDB._get(self._db, self.__get_size(), index, self.__value_type)
 
     def __contains__(self, item: V):
         for e in self:
@@ -314,8 +317,14 @@ class ArrayDB(object):
                 return True
         return False
 
-    @classmethod
-    def _get(cls, db: Union['IconScoreDatabase', 'IconScoreSubDatabase'], size: int, index: int, value_type: type) -> V:
+    @staticmethod
+    def __is_defective_revision():
+        context = ContextContainer._get_context()
+        revision = context.revision
+        return context.type == IconScoreContextType.INVOKE and revision < Revision.THREE.value
+
+    @staticmethod
+    def _get(db: Union['IconScoreDatabase', 'IconScoreSubDatabase'], size: int, index: int, value_type: type) -> V:
         if not isinstance(index, int):
             raise InvalidParamsException('Invalid index type: not an integer')
 
@@ -329,10 +338,10 @@ class ArrayDB(object):
 
         raise InvalidParamsException('ArrayDB out of index')
 
-    @classmethod
-    def _get_generator(cls, db: Union['IconScoreDatabase', 'IconScoreSubDatabase'], size: int, value_type: type):
+    @staticmethod
+    def _get_generator(db: Union['IconScoreDatabase', 'IconScoreSubDatabase'], size: int, value_type: type):
         for index in range(size):
-            yield cls._get(db, size, index, value_type)
+            yield ArrayDB._get(db, size, index, value_type)
 
 
 class VarDB(object):
