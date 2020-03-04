927f69ec5a282201c2e7089956147244a9188488
Chiwon Cho <goldworm@iconloop.com>
2020-03-04 19:52:10 +0900

    Add sha_256 function for SCORE development
    * Add a related unittest

--- iconservice/__init__.py
+++ iconservice/__init__.py
@@ -24,7 +24,7 @@ from .base.exception import IconScoreException
 from .icon_constant import IconServiceFlag
 from .iconscore.icon_container_db import VarDB, DictDB, ArrayDB
 from .iconscore.icon_score_base import interface, eventlog, external, payable, IconScoreBase, IconScoreDatabase
-from .iconscore.icon_score_base2 import (InterfaceScore, revert, sha3_256, json_loads, json_dumps,
+from .iconscore.icon_score_base2 import (InterfaceScore, revert, sha3_256, sha_256, json_loads, json_dumps,
                                          get_main_prep_info, get_sub_prep_info, recover_key,
                                          create_address_with_key, create_interface_score)
 from .iconscore.icon_system_score_base import IconSystemScoreBase
