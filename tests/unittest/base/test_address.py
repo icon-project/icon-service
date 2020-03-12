f5fd864889d011ff1b1317b9e1d4360a28214286
inwonkim <inwonkim@iconloop.com>
2020-03-12 17:07:11 +0900

    Address * unify test_get_prefix_EOA and test_get_prefix_CONTRACT Rewrite unit test * Address * hash_generator * deploy * block

--- tests/unittest/base/test_address.py
+++ tests/unittest/base/test_address.py
@@ -25,167 +25,92 @@ from iconservice.base.exception import ExceptionCode
 from tests import create_address
 
 
-def test_get_prefix_EOA():
-    addr = create_address()
-    assert str(addr.prefix) == ICON_EOA_ADDRESS_PREFIX
-    assert addr.prefix == AddressPrefix.EOA
+class TestAddress:
+    @pytest.mark.parametrize("prefix,expected_prefix,expected_prefix_constant",
+                             [(0, AddressPrefix.EOA, ICON_EOA_ADDRESS_PREFIX)],
+                             (1, AddressPrefix.CONTRACT, ICON_CONTRACT_ADDRESS_PREFIX))
+    def test_get_prefix(self, prefix, expected_prefix, expected_prefix_constant):
+        addr = create_address(prefix=prefix)
+        assert str(addr.prefix) == expected_prefix_constant
+        assert addr.prefix == expected_prefix
 
+    @pytest.mark.parametrize("prefix,data1,data2", [(0, b'addr1', b'addr2'), (1, b'addr1', b'addr2')])
+    def test_equality(self, prefix, data1, data2):
+        addr1 = create_address(prefix=prefix, data=data1)
+        addr2 = create_address(prefix=prefix, data=data1)
+        addr3 = create_address(prefix=prefix, data=data2)
+        assert addr1 == addr2
+        assert addr1 != addr3
 
-def test_get_prefix_CONTRACT():
-    addr = create_address(1)
-    assert str(addr.prefix) == ICON_CONTRACT_ADDRESS_PREFIX
-    assert addr.prefix == AddressPrefix.CONTRACT
+    @pytest.mark.parametrize("prefix,data", [(0, b'addr'), (1, b'addr')])
+    def test_hash(self, prefix, data):
+        addr1 = create_address(prefix=prefix, data=data)
+        addr2 = create_address(prefix=prefix, data=data)
+        assert addr1 == addr2
+        assert hash(addr1) == hash(addr2)
+        assert addr1 is not addr2
 
+        table = {addr1: 100}
+        assert table[addr1] == table[addr2]
 
-def test_eq_EOA():
-    addr1 = create_address(data=b'addr')
-    addr2 = create_address(data=b'addr')
-    assert addr1 == addr2
+    @pytest.mark.parametrize("prefix", [0, 1])
+    def test_address_from_to_bytes(self, prefix):
+        addr1 = create_address(prefix)
+        buf = addr1.to_bytes()
+        addr2 = Address.from_bytes(buf)
+        assert addr1 == addr2
 
+    def test_address_from_to_bytes_OTHER(self):
+        addr1 = create_address()
+        buf = addr1.to_bytes()
+        prefix: int = 0
+        prefix_buf: bytes = prefix.to_bytes(1, 'big')
+        buf = prefix_buf + buf
+        addr2 = Address.from_bytes(buf)
+        assert addr1 == addr2
 
-def test_eq_CONTRACT():
-    addr1 = create_address(prefix=1, data=b'addr')
-    addr2 = create_address(prefix=1, data=b'addr')
-    assert addr1 == addr2
+    @pytest.mark.parametrize("prefix", [0, 1])
+    def test_address_from_to_string(self, prefix):
+        addr1 = create_address(prefix=prefix)
+        buf = str(addr1)
+        addr2 = Address.from_string(buf)
+        assert addr1 == addr2
 
+    @pytest.mark.parametrize("address", ["hx123456", bytes.hex(GOVERNANCE_SCORE_ADDRESS.body)])
+    def test_from_string_invalid(self, address):
+        with pytest.raises(BaseException) as e:
+            Address.from_string(address)
+        assert e.value.code == ExceptionCode.INVALID_PARAMETER
+        assert e.value.message == "Invalid address"
 
-def test_ne_EOA():
-    addr1 = create_address(data=b'addr1')
-    addr2 = create_address(data=b'addr2')
-    assert addr1 != addr2
+    def test_is_icon_address_valid_valid_case(self):
+        address: str = str(create_address())
+        assert is_icon_address_valid(address)
 
+    @pytest.mark.parametrize("address_string", [f"{'1234'*10}", "12", 1234, f"0x{'1234'*10}"])
+    def test_is_icon_address_valid_invalid_cases(self, address_string):
+        """without prefix, shorten data, wrong prefix data"""
+        assert is_icon_address_valid(address_string) is False
 
-def test_ne_CONTRACT():
-    addr1 = create_address(prefix=1, data=b'addr1')
-    addr2 = create_address(prefix=1, data=b'addr2')
-    assert addr1 != addr2
+    def test_split_icon_address(self):
+        address = create_address()
+        prefix, body = split_icon_address(str(address))
+        assert prefix == str(address.prefix)
+        assert body == bytes.hex(address.body)
 
+    def test_prefix_and_int(self):
+        assert Address.from_prefix_and_int(AddressPrefix.CONTRACT, 0) == ZERO_SCORE_ADDRESS
+        assert Address.from_prefix_and_int(AddressPrefix.CONTRACT, 1) == GOVERNANCE_SCORE_ADDRESS
+        assert str(Address.from_prefix_and_int(AddressPrefix.EOA, 10)) == "hx000000000000000000000000000000000000000a"
+        assert str(Address.from_prefix_and_int(AddressPrefix.CONTRACT, 1024)) == \
+               "cx0000000000000000000000000000000000000400"
 
-def test_hash_EOA():
-    addr1 = create_address(data=b'addr')
-    addr2 = create_address(data=b'addr')
-    assert addr1 == addr2
-    assert hash(addr1) == hash(addr2)
-    assert addr1 is not addr2
+    @pytest.mark.parametrize("prefix", [prefix for prefix in AddressPrefix])
+    def test_from_bytes_including_prefix(self, prefix):
+        value: int = random.randint(0, 0xffffffff)
+        input_data: bytes = value.to_bytes(4, 'big')
+        data: bytes = hashlib.sha3_256(input_data).digest()
 
-    table = {addr1: 100}
-    assert table[addr1] == table[addr2]
-
-
-def test_hash_CONTRACT():
-    addr1 = create_address(prefix=1, data=b'addr')
-    addr2 = create_address(prefix=1, data=b'addr')
-    assert addr1 == addr2
-    assert hash(addr1) == hash(addr2)
-    assert addr1 is not addr2
-
-    table = {addr1: 100}
-    assert table[addr1] == table[addr2]
-
-
-def test_address_from_to_bytes_EOA():
-    addr1 = create_address()
-    buf = addr1.to_bytes()
-    addr2 = Address.from_bytes(buf)
-    assert addr1 == addr2
-
-
-def test_address_from_to_bytes_CONTRACT():
-    addr1 = create_address(prefix=1)
-    buf = addr1.to_bytes()
-    addr2 = Address.from_bytes(buf)
-    assert addr1 == addr2
-
-
-def test_address_from_to_bytes_OTHER():
-    addr1 = create_address()
-    buf = addr1.to_bytes()
-    prefix: int = 0
-    prefix_buf: bytes = prefix.to_bytes(1, 'big')
-    buf = prefix_buf + buf
-    addr2 = Address.from_bytes(buf)
-    assert addr1 == addr2
-
-
-def test_address_from_to_string_EOA():
-    addr1 = create_address()
-    buf = str(addr1)
-    addr2 = Address.from_string(buf)
-    assert addr1 == addr2
-
-
-def test_address_from_to_string_EOA2():
-    addr1 = create_address()
-    buf = bytes.hex(addr1.body)
-    with pytest.raises(BaseException) as e:
-        Address.from_string(buf)
-    assert e.value.code == ExceptionCode.INVALID_PARAMETER
-    assert e.value.message == 'Invalid address'
-
-
-def test_address_from_to_string_CONTRACT():
-    addr1 = create_address(prefix=1)
-    buf = str(addr1)
-    addr2 = Address.from_string(buf)
-    assert addr1 == addr2
-
-
-def test_is_icon_address_valid():
-    address: str = str(create_address())
-    assert is_icon_address_valid(address)
-
-    # Remove prefix 'hx'
-    a = address[2:]
-    assert is_icon_address_valid(a) is False
-
-    # short address
-    a = address[:-1]
-    assert is_icon_address_valid(a) is False
-
-    # wrong param prefix
-    assert is_icon_address_valid(1234) is False
-
-    # wrong hexadecimal format
-    assert is_icon_address_valid("0x00c3f694d84074f9145cd0bfa497290ce2d8052f") is False
-
-
-def test_split_icon_address():
-    address = create_address()
-    prefix, body = split_icon_address(str(address))
-    assert prefix == str(address.prefix)
-    assert body == bytes.hex(address.body)
-
-
-def test_prefix_and_int():
-    assert Address.from_prefix_and_int(AddressPrefix.CONTRACT, 0) == ZERO_SCORE_ADDRESS
-    assert Address.from_prefix_and_int(AddressPrefix.CONTRACT, 1) == GOVERNANCE_SCORE_ADDRESS
-    assert str(Address.from_prefix_and_int(AddressPrefix.EOA, 10)) == "hx000000000000000000000000000000000000000a"
-    assert str(Address.from_prefix_and_int(AddressPrefix.CONTRACT, 1024)) == \
-           "cx0000000000000000000000000000000000000400"
-
-
-def test_malformed_address():
-    address: str = "hx123456"
-    addr = MalformedAddress.from_string(address)
-    assert str(addr) == address
-
-
-def test_invalid_address():
-    address: str = "hx123456"
-    with pytest.raises(BaseException) as e:
-        Address.from_string(address)
-    assert e.value.code ==  ExceptionCode.INVALID_PARAMETER
-    assert e.value.message == "Invalid address"
-
-
-def test_from_bytes_including_prefix():
-    address_prefixes = [AddressPrefix.EOA, AddressPrefix.CONTRACT]
-
-    value: int = random.randint(0, 0xffffffff)
-    input_data: bytes = value.to_bytes(4, 'big')
-    data: bytes = hashlib.sha3_256(input_data).digest()
-
-    for prefix in address_prefixes:
         body: bytes = data[-20:]
         assert 20 == len(body)
 
@@ -195,7 +120,7 @@ def test_from_bytes_including_prefix():
         assert prefix == address.prefix
         assert body == address.body
 
-    for prefix in address_prefixes:
+        # test with longer or shorter data
         size = random.randint(1, 32)
         if size == 20:
             size += 1
@@ -210,14 +135,13 @@ def test_from_bytes_including_prefix():
         address = Address.from_bytes_including_prefix(body)
         assert address is None
 
+    @pytest.mark.parametrize("prefix", [prefix for prefix in AddressPrefix])
+    def test_to_bytes_including_prefix(self, prefix):
+        value: int = random.randint(0, 0xffffffff)
+        input_data: bytes = value.to_bytes(4, 'big')
+        data: bytes = hashlib.sha3_256(input_data).digest()
+        body: bytes = data[-20:]
 
-def test_to_bytes_including_prefix():
-    value: int = random.randint(0, 0xffffffff)
-    input_data: bytes = value.to_bytes(4, 'big')
-    data: bytes = hashlib.sha3_256(input_data).digest()
-    body: bytes = data[-20:]
-
-    for prefix in [AddressPrefix.EOA, AddressPrefix.CONTRACT]:
         address = Address(prefix, body)
         address_bytes: bytes = address.to_bytes_including_prefix()
 
@@ -225,3 +149,20 @@ def test_to_bytes_including_prefix():
         assert isinstance(address_bytes, bytes)
         assert 21 == len(address_bytes)
         assert expected_bytes == address_bytes
+
+
+class TestMalformedAddress:
+    @pytest.mark.parametrize("address_string", ['', '123124124125',
+                                                'bf85fac2d1b507a2db9ce9526e6d91476f16a2d269f51636f9c4b2d512017faf'])
+    def test_from_string_without_prefix(self, address_string):
+        address = MalformedAddress.from_string(address_string)
+        assert address.prefix == AddressPrefix.EOA
+        assert address.body == bytes.fromhex(address_string)
+        assert str(address) == f'hx{address_string}'
+
+    def test_from_string_with_prefix(self):
+        text = 'hxa23651905d221dd36b'
+        short_address = MalformedAddress.from_string(text)
+        assert short_address.prefix == AddressPrefix.EOA
+        assert str(short_address) == text
+        assert short_address.body == bytes.fromhex('a23651905d221dd36b')
