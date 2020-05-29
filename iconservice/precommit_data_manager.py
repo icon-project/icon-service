b48cd06526d20cc3de4e2f9f031576745ca130c8
soobokjin <soobokjin@icon.foundation>
2020-05-29 12:27:07 +0900

    Remove log

--- iconservice/precommit_data_manager.py
+++ iconservice/precommit_data_manager.py
@@ -57,11 +57,6 @@ def write_precommit_data_to_file(precommit_data: 'PrecommitData', path: str):
             f.write(_convert_rc_block_batch_to_string(precommit_data.rc_block_batch))
         except Exception as e:
             f.write(f"Exception raised during writing the precommit-data: {e}")
-            Logger.warning(
-                tag=_TAG,
-                msg=f"Exception raised during writing the precommit-data: {e}"
-            )
-            pass
 
 
 def _convert_block_batch_to_string(block_batch: 'BlockBatch') -> str:
