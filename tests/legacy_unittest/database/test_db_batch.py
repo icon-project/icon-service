2a14e66db73ffa521f906876ddda7453d8cdd00f
soobokjin <soobokjin@iconloop.com>
2020-06-05 17:35:22 +0900

    Supplement unit tests
    Fix the unittests

--- tests/legacy_unittest/database/test_db_batch.py
+++ tests/legacy_unittest/database/test_db_batch.py
@@ -84,45 +84,45 @@ class TestBatch(unittest.TestCase):
 
         key = create_hash_256()
         tx_batch = TransactionBatch(create_hash_256())
-        tx_batch[key] = (b'value', True)
+        tx_batch[key] = TransactionBatchValue(b'value', True)
         self.block_batch.update(tx_batch)
-        self.assertEqual((b'value', True), self.block_batch[key])
+        self.assertEqual(TransactionBatchValue(b'value', True), self.block_batch[key])
 
         value = 100
-        tx_batch[key] = (value.to_bytes(8, byteorder), True)
+        tx_batch[key] = TransactionBatchValue(value.to_bytes(8, byteorder), True)
         self.block_batch.update(tx_batch)
         self.assertEqual(
             value,
-            int.from_bytes(self.block_batch[key][0], byteorder))
+            int.from_bytes(self.block_batch[key].value, byteorder))
 
     def test_put_tx_batch(self):
         tx_hash = create_hash_256()
         tx_batch = TransactionBatch(tx_hash)
 
         key = create_hash_256()
-        tx_batch[key] = (b'value', True)
+        tx_batch[key] = TransactionBatchValue(b'value', True)
 
         key0 = create_hash_256()
-        tx_batch[key0] = (b'value0', True)
+        tx_batch[key0] = TransactionBatchValue(b'value0', True)
         key1 = create_hash_256()
-        tx_batch[key1] = (b'value1', True)
+        tx_batch[key1] = TransactionBatchValue(b'value1', True)
         key2 = create_hash_256()
-        tx_batch[key2] = (b'value2', True)
+        tx_batch[key2] = TransactionBatchValue(b'value2', True)
 
         self.assertEqual(4, len(tx_batch))
 
         tx_batch_2 = TransactionBatch(tx_hash)
-        tx_batch_2[key] = (b'haha', True)
+        tx_batch_2[key] = TransactionBatchValue(b'haha', True)
         self.block_batch.update(tx_batch_2)
         self.assertEqual(1, len(self.block_batch))
 
         self.block_batch.update(tx_batch)
 
         self.assertEqual(4, len(self.block_batch))
-        self.assertEqual((b'value0', True), self.block_batch[key0])
-        self.assertEqual((b'value1', True), self.block_batch[key1])
-        self.assertEqual((b'value2', True), self.block_batch[key2])
-        self.assertEqual((b'value', True), self.block_batch[key])
+        self.assertEqual(TransactionBatchValue(b'value0', True).value, self.block_batch[key0].value)
+        self.assertEqual(TransactionBatchValue(b'value1', True).value, self.block_batch[key1].value)
+        self.assertEqual(TransactionBatchValue(b'value2', True).value, self.block_batch[key2].value)
+        self.assertEqual(TransactionBatchValue(b'value', True).value, self.block_batch[key].value)
 
     def test_digest(self):
         block_batch = self.block_batch
@@ -173,3 +173,28 @@ class TestBatch(unittest.TestCase):
         expected = sha3_256(b'|'.join(data))
         ret = block_batch.digest()
         self.assertEqual(expected, ret)
+
+    def test_block_batch_update_tx_index(self):
+        block_batch = self.block_batch
+
+        overwrite_key = create_hash_256()
+        last_value = None
+
+        for i in range(3):
+            last_value = b'value' + i.to_bytes(1, 'big')
+            tx_batch = TransactionBatch(create_hash_256())
+            tx_batch[overwrite_key] = TransactionBatchValue(last_value, True, i)
+            block_batch.update(tx_batch)
+        tx_batch = TransactionBatch(create_hash_256())
+        tx_batch[overwrite_key] = TransactionBatchValue(b'reverted_value1', True, 3)
+        tx_batch.revert_call()
+        block_batch.update(tx_batch)
+
+        tx_batch = TransactionBatch(create_hash_256())
+        tx_batch[overwrite_key] = TransactionBatchValue(b'reverted_value2', True, 4)
+        tx_batch.clear()
+        block_batch.update(tx_batch)
+
+        actual_overwrite_value: 'TransactionBatchValue' = block_batch.get(overwrite_key)
+        assert actual_overwrite_value.value == last_value
+        assert actual_overwrite_value.tx_indexes == [0, 1, 2]
