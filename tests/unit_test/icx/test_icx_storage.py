69b7da50c7607cfb6fd3770e60bb14f66fd14bad
inwonkim <inwonkim@icon.foundation>
2020-08-26 22:50:14 +0900

    Write test for getting Account in IcxStorage

--- tests/unit_test/icx/test_icx_storage.py
+++ tests/unit_test/icx/test_icx_storage.py
@@ -13,18 +13,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import shutil
 from unittest.mock import PropertyMock
 
 import pytest
 
 from iconservice import Address
 from iconservice.base.block import Block
+from iconservice.database.db import ContextDatabase
 from iconservice.icon_constant import Revision, IconScoreContextType
 from iconservice.iconscore.context.context import ContextContainer
 from iconservice.iconscore.icon_score_context import IconScoreContext
 from iconservice.icx.coin_part import CoinPart, CoinPartFlag, CoinPartType
 from iconservice.icx.icx_account import Account
 from iconservice.icx.stake_part import StakePart
+from iconservice.icx.storage import Storage, Intent
+from iconservice.utils import ContextStorage
 
 ADDRESS = Address.from_string(f"hx{'1234'*10}")
 UNSTAKE_LOCK_PERIOD = 20
@@ -41,46 +45,45 @@ def context():
     ContextContainer._pop_context()
 
 
-class TestAccount:
+@pytest.fixture(scope="function")
+def storage(context):
+    db_name = 'icx.db'
+    db = ContextDatabase.from_path(db_name)
+    storage = Storage(db)
+    context.storage = ContextStorage(icx=storage)
+    storage.open(context)
+    yield storage
+    storage.close(context)
+    shutil.rmtree(db_name)
 
-    @pytest.mark.parametrize("revision", [
-        revision.value for revision in Revision if revision.value >= Revision.MULTIPLE_UNSTAKE.value
+
+class TestIcxStorage:
+
+    @pytest.mark.parametrize("unstakes_info, current_block_height, flag, expected_balance", [
+        ([], 20, CoinPartFlag.NONE, 100),
+        ([[10, 20]], 5, CoinPartFlag.HAS_UNSTAKE, 100),
+        ([[10, 20]], 25, CoinPartFlag.NONE, 110),
+        ([[10, 20], [10, 30]], 15, CoinPartFlag.HAS_UNSTAKE, 100),
+        ([[10, 20], [10, 30]], 25, CoinPartFlag.NONE, 110),
+        ([[10, 20], [10, 30]], 35, CoinPartFlag.NONE, 120),
     ])
-    @pytest.mark.parametrize("unstakes_info, current_block_height, has_unstake", [
-        ([], 20, False),
-        ([[10, 20]], 5, True),
-        ([[10, 20]], 25, False),
-        ([[10, 20], [10, 30]], 15, True),
-        ([[10, 20], [10, 30]], 25, False),
-        ([[10, 20], [10, 30]], 35, False),
-    ])
-    def test_normalize(
-            self, context, mocker, revision, unstakes_info, current_block_height, has_unstake):
+    def test_get_account(
+            self, storage, context, mocker, unstakes_info, current_block_height, flag, expected_balance):
+        revision = Revision.FIX_BALANCE_BUG.value
         mocker.patch.object(IconScoreContext, "revision", PropertyMock(return_value=revision))
-        stake, balance = 100, 0
+        stake, balance = 100, 100
         coin_part = CoinPart(CoinPartType.GENERAL, CoinPartFlag.NONE, balance)
-        stake_part = StakePart(stake=stake, unstake=0, unstake_block_height=0)
+        coin_part.set_dirty(True)
+        stake_part = StakePart(stake=stake, unstake=0, unstake_block_height=0, unstakes_info=unstakes_info)
+        stake_part.set_dirty(True)
         account = Account(
             ADDRESS, current_block_height, revision, coin_part=coin_part, stake_part=stake_part)
+        account.coin_part._flags = flag
+        context.block._height = current_block_height
+        storage.put_account(context, account)
 
-        for info in unstakes_info:
-            context.block._height = info[1] - UNSTAKE_LOCK_PERIOD
-            stake = stake - info[0]
-            # set_stake method refer account._current_block_height
-            account._current_block_height = context.block.height
-            account.set_stake(context, stake, UNSTAKE_LOCK_PERIOD)
-
-        stake_part = account.stake_part
         remaining_unstakes = [unstake_info for unstake_info in unstakes_info if unstake_info[1] > current_block_height]
-        expired_unstake = sum((unstake_info[0] for unstake_info in unstakes_info
-                               if unstake_info[1] < current_block_height))
-        balance = expired_unstake
 
-        account = Account(ADDRESS, current_block_height, revision, coin_part=coin_part, stake_part=stake_part)
-        if revision >= Revision.FIX_BALANCE_BUG.value:
-            has_unstake = False
-
-        assert stake == account.stake
-        assert balance == account.balance
-        assert remaining_unstakes == account.unstakes_info
-        assert (CoinPartFlag.HAS_UNSTAKE in account.coin_part.flags) == has_unstake
+        account = storage.get_account(context, ADDRESS)
+        assert account.balance == expected_balance
+        assert account.unstakes_info == remaining_unstakes
